0b284b0fe9b76b955eedd90e2f40b9f412d47e04
Richard Cook <wealthychef@gmail.com>
2009-06-19 22:39:59 +0000

    move user interface list to ui.cpp where it sort of belongs.

--- src/blockbuster/ui.h
+++ src/blockbuster/ui.h
@@ -11,15 +11,10 @@
     MovieStatus (*Initialize)(struct Canvas *canvas, const ProgramOptions *options,
             qint32 uiData, const RendererSpecificGlue *configurationData);
 
-    /* This routine is only called if the main program is called
-     * without any movie arguments.  If unimplemented or if it
-     * returns NULL, the main program will exit.  Otherwise,
-     * it will continue as though it had been passed the
-     * returned string.
-     *
-     * The returned string must be allocated with malloc(); it
-     * will be released with free().
-     */
+    /*!
+      ChooseFile: The returned string must be allocated with malloc(); it
+      will be released with free().
+    */
     char *(*ChooseFile)(const ProgramOptions *options);
 
   } ;
