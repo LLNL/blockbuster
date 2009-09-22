b66623c8e29a2c5e3f73e32e8e7cb93572057e47
Richard Cook <wealthychef@gmail.com>
2009-09-22 23:39:09 +0000

    2.6.4b -- see changelog

--- src/blockbuster/pnm.h
+++
@@ -1,5 +0,0 @@
-#ifndef BBPNM_H
-#define BBPNM_H
-#include "frames.h"
-FrameList *pnmGetFrameList(const char *filename);
-#endif
--- src/config/version.h
+++
@@ -1,3 +0,0 @@
-#ifndef BLOCKBUSTER_VERSION
-#define BLOCKBUSTER_VERSION "2.6.3a -- June 19, 2009"
-#endif
