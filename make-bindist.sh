6cd4a8a5d76e92837c03d2c8422aaffbef2ba9cc
Richard Cook <wealthychef@gmail.com>
2011-10-28 22:10:59 +0000

    set RPATH upon building, not during bindist, so dependency on huge Qt installations goes away

--- make-bindist.sh
+++ make-bindist.sh
@@ -13,13 +13,6 @@ if [ $(uname) == Linux ]; then
             export INSTALL_DIR=linux-basic-nodmx 
         fi
         INSTALL_DIR=$(pwd)/$INSTALL_DIR remake.sh $buildarg || errexit "build failed for $INSTALL_DIR"
-        echo "Setting rpath for installed executables to find Qt libraries." 
-        for exe in ${INSTALL_DIR}/bin/blockbuster ${INSTALL_DIR}/bin/sidecar; do 
-            cp -f $(ldd $exe | grep Qt | awk '{print $3}') ${INSTALL_DIR}/lib;
-            rpath='${ORIGIN}/../lib:'$(chrpath -l $exe | awk ' {print $2}' | sed 's/RPATH=//' | sed 's~[^:]*Trolltech[^:]*~:~' | sed "s:$INSTALL_DIR::")
-            rpath=$(echo $rpath | sed 's~::~:~g' )
-            chrpath -r $rpath $exe 
-        done
         cp -fp bindist-src/README-install.txt bindist-src/install.sh ${INSTALL_DIR}
         tar -czf ${INSTALL_DIR}.tgz ${INSTALL_DIR}    
         rm -rf ${INSTALL_DIR}
