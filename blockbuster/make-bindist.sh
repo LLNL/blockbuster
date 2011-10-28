c1ea971c12608a53224b46b241498b289cf62436
Richard Cook <wealthychef@gmail.com>
2011-10-28 00:30:17 +0000

    label linux and mac bindists differently

--- blockbuster/make-bindist.sh
+++ blockbuster/make-bindist.sh
@@ -5,14 +5,14 @@ if [ $(uname) == Linux ]; then
     cp -rp ${INSTALL_DIR}/{bin,lib,man,share,doc} blockbuster-install
     sed "s:OLD_INSTALL_DIR=.*:OLD_INSTALL_DIR=${INSTALL_DIR}:" install.sh > blockbuster-install/install.sh
     chmod 755 blockbuster-install/install.sh
-    tar -czf blockbuster-install.tgz blockbuster-install
+    tar -czf blockbuster-install-linux.tgz blockbuster-install
     rm -rf blockbuster-install
     echo "Created blockbuster-install.tgz containing Linux installer in current directory." 
 elif [ $(uname) == Darwin ]; then 
     macdeployqt src/blockbuster/blockbuster.app -dmg
     macdeployqt src/blockbuster/sidecar/sidecar.app -dmg
     mv src/blockbuster/blockbuster.dmg src/blockbuster/sidecar/sidecar.dmg ./
-    tar -czf blockbuster-install.tgz blockbuster.dmg sidecar.dmg
+    tar -czf blockbuster-install-mac.tgz blockbuster.dmg sidecar.dmg
     echo "Created blockbuster-install.tgz containing blocbuster.dmg and sidecar.dmg in current directory." 
 else
     echo "Error: Unrecognized uname results: $(uname).  Nothing done."
