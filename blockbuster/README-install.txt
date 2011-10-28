97ff9d48beef0fe237a3c8dc03d703dbb8a87e2a
Richard Cook <wealthychef@gmail.com>
2011-10-28 00:49:44 +0000

    include a README in binary installers.

---
+++ blockbuster/README-install.txt
@@ -0,0 +1,8 @@
+# This file is to be included in the Mac and Linux installer tarballs.  
+
+LINUX INSTALLATION: 
+Run the install.sh script and give it an INSTALL_DIR, either through the environment variable INSTALL_DIR, via command line argument, or in response to the interactive prompt.  $INSTALL_DIR/bin/blockbuster and $INSTALL_DIR/bin/sidecar will be installed, along with libraries and documentation as needed.  
+
+MAC INSTALLATION: 
+Decompress install-blockbuster-mac.tgz and you will find blockbuster.dmg and sidecar.dmg.  Double click on them and drag the executables where you need to have them. 
+
--- blockbuster/make-bindist.sh
+++ blockbuster/make-bindist.sh
@@ -2,7 +2,7 @@
 set -xv
 if [ $(uname) == Linux ]; then 
     rm -rf blockbuster-install && 	mkdir blockbuster-install
-    cp -rp ${INSTALL_DIR}/{bin,lib,man,share,doc} blockbuster-install
+    cp -rp ${INSTALL_DIR}/{bin,lib,man,share,doc} README-install.txt blockbuster-install
     sed "s:OLD_INSTALL_DIR=.*:OLD_INSTALL_DIR=${INSTALL_DIR}:" install.sh > blockbuster-install/install.sh
     chmod 755 blockbuster-install/install.sh
     tar -czf blockbuster-install-linux.tgz blockbuster-install
@@ -12,7 +12,7 @@ elif [ $(uname) == Darwin ]; then
     macdeployqt src/blockbuster/blockbuster.app -dmg
     macdeployqt src/blockbuster/sidecar/sidecar.app -dmg
     mv src/blockbuster/blockbuster.dmg src/blockbuster/sidecar/sidecar.dmg ./
-    tar -czf blockbuster-install-mac.tgz blockbuster.dmg sidecar.dmg
+    tar -czf blockbuster-install-mac.tgz blockbuster.dmg sidecar.dmg README-install.txt
     echo "Created blockbuster-install.tgz containing blocbuster.dmg and sidecar.dmg in current directory." 
 else
     echo "Error: Unrecognized uname results: $(uname).  Nothing done."
