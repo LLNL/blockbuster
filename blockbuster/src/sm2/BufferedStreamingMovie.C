355bf50732d94b747a741d4e7d13fa88b01b1d5f
Richard Cook <wealthychef@gmail.com>
2012-04-19 19:13:20 +0000

    simplified

--- blockbuster/src/sm2/BufferedStreamingMovie.C
+++ blockbuster/src/sm2/BufferedStreamingMovie.C
@@ -3,34 +3,13 @@
 
 using namespace boost; 
 
-// Called by Render thread when it needs an image
-bool RenderBuffer::RequestImage(const Image &image, CImg<unsigned char> &cimg) {
-  lock_guard<mutex> lock(mMutex); 
-  mRequestedImage = image; 
+
+
+bool BufferedStreamingMovie::DisplayFrame(uint32_t framenum){
+  // update the current image: 
   
-  // find image in buffer (or sleep)
-  /* while (!haveImage(image)) {
-    mWaitingForImage.wait(lock); 
-  }
-  */
+  // if the front buffer has the image, then display it. 
+
+  // else, 
   return false; 
-}
-
-
-bool RenderBuffer::DepositFrame(RenderedImage &image) {
-  {
-    lock_guard<mutex> lock(mMutex); // need exclusive access for writing
-    // AddImageToBuffer(image); 
-  }
-  mWaitingForImage.notify_one();// only the Renderer might be waiting 
-  return false; 
-}
-
-
-
-
-void DoubleIOBuffer::RequestImage(const Image &image) {
-    lock_guard<shared_mutex> lock(mMutex); 
-    mRequestedImage = image; 
-    return; 
 }
