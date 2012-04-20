ad91840882f21061f4eaacd4c9d9c54b4f56d1d1
Richard Cook <wealthychef@gmail.com>
2012-04-20 01:17:20 +0000

    compiles, contains some threading semantics and removed Reader and Decompressor classes as they are useless and can be done in a single function I hope

--- src/sm2/BufferedStreamingMovie.C
+++ src/sm2/BufferedStreamingMovie.C
@@ -1,22 +1,64 @@
 
 #include "BufferedStreamingMovie.h"
+#include <boost/make_shared.hpp>
 
-using namespace boost; 
+// using namespace boost; 
 
 bool BufferedStreamingMovie::ReadHeader(void) {  
-  StreamingMovie::ReadHeader(); 
+  // remove all instructions to do any work from the IOBuffer
+  mKeepDecompressing = false; 
+  mKeepReading = false; 
+
+  if (mReaderThread.get_id() != boost::thread::id()) {
+    mReaderThread.join(); 
+  } 
+ 
+  int id = 0; 
+  while (id < mDecompressorThreads.size()) {
+    if (mDecompressorThreads[id]->get_id() != boost::thread::id()) {
+      mDecompressorThreads[id]->join(); 
+    }
+    ++id; 
+  } 
+  mDecompressorThreads.clear(); 
+
+  return  StreamingMovie::ReadHeader(); 
+ 
+} 
+
+/*
+  Start the Reader thread and the given number of decompressors. 
+ */ 
+void BufferedStreamingMovie::StartThreads(int numthreads){
+
+  boost::lock_guard<boost::shared_mutex> lock(mRenderBuffer.mMutex); 
   mRenderBuffer.SetFrameSize(mFrameSizes[mLOD][0]*mFrameSizes[mLOD][1]*3); 
+
+  // start reading frames... 
+  mReaderThread = boost::thread(&BufferedStreamingMovie::ReaderThread, this); 
+  int id = 0; 
+  while (id < mNumDecompressorThreads) {
+    //boost::thread theThread = ;
+    mDecompressorThreads.push_back(boost::make_shared<boost::thread>(&BufferedStreamingMovie::DecompressorThread, this, id) ); 
+    id++; 
+  }
+
 }
 
 
 bool BufferedStreamingMovie::DisplayFrame(uint32_t framenum){
-  // update the current image: 
-  mCurrentImage.mFrameNumber = framenum; 
 
-  // if the render buffer has the image, then display it.   
-  if (mRenderBuffer.mFrameSlots[framenum]) {
-    mDisplayer.display(mRenderBuffer.mFrameSlots[framenum]->mCimg); 
+  bool found=false; 
+  mCurrentImage.mFrameNumber = framenum; // no mutex required
+
+  {   // if the render buffer has the image, then display it.  
+    boost::lock_guard<boost::shared_mutex> lock(mRenderBuffer.mMutex); 
+    if (mRenderBuffer.mFrameSlots[framenum]) {
+      mDisplayer.display(mRenderBuffer.mFrameSlots[framenum]->mCimg); 
+      found=true; 
+    } 
   }
-  // else, 
-  return false; 
+  mRenderBuffer.mCondition.notify_all(); 
+  mDoubleIOBuffer.mCondition.notify_all(); 
+  return found; 
 }
