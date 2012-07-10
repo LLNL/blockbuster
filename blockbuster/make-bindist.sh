e4fccfadd002c5af03bca144857023ecb5ee41dc
Richard Cook <wealthychef@gmail.com>
2012-07-10 20:25:56 +0000

    use tmp dir

--- blockbuster/make-bindist.sh
+++ blockbuster/make-bindist.sh
@@ -4,25 +4,32 @@ function errexit() {
     echo $1
     exit 1
 }
+tmpdir=/tmp/$(whoami)
+mkdir -p $tmpdir
+
 version="$(cat src/config/versionstring.txt)"
 if [ $(uname) == Linux ]; then 
     for buildarg in all nodmx; do 
         if [ $buildarg == all ]; then
-            export INSTALL_DIR=linux-dmx-v$version-$(uname -r)
+            export INSTALL_NAME=linux-dmx-v$version-$(uname -r)
         else
-            export INSTALL_DIR=linux-basic-nodmx-v$version-$(uname -r)
+            export INSTALL_NAME=linux-basic-nodmx-v$version-$(uname -r)
         fi
-        INSTALL_DIR=$(pwd)/$INSTALL_DIR remake.sh $buildarg || errexit "build failed for $INSTALL_DIR"
+        export INSTALL_DIR=$tmpdir/$INSTALL_NAME
+        INSTALL_DIR=$INSTALL_DIR remake.sh $buildarg || errexit "build failed for $INSTALL_DIR"
         cp -fp bindist-src/README-install.txt bindist-src/install.sh ${INSTALL_DIR}
-        tar -czf ${INSTALL_DIR}.tgz ${INSTALL_DIR}    
+        tarball=$tmpdir/${INSTALL_NAME}.tgz
+        pushd ${INSTALL_DIR}/..
+        tar -czf $tarball ${INSTALL_NAME}
+        popd
         rm -rf ${INSTALL_DIR}
-        echo "Created ${INSTALL_DIR}.tgz containing Linux installer in current directory." 
+        mv $tarball .
+        echo "Created $(basename ${tarball}) containing Linux installer in current directory." 
     done
 elif [ $(uname) == Darwin ]; then 
     # make || errexit "make failed"
     cp $INSTALL_DIR/bin/blockbuster.dmg $INSTALL_DIR/bin/sidecar.dmg ./
-    tar -czf blockbuster-install-mac-v$version-$(uname -r).tgz blockbuster.dmg sidecar.dmg bindist-src/README-install.txt || errexit "Cannot tar up the files" 
-    echo "Created blockbuster-install-mac-v$version.tgz containing blocbuster.dmg and sidecar.dmg in current directory." 
+    tar -czf blockbuster-install-mac-v$version-$(uname -r).tgz blockbuster.dmg sidecar.dmg bindist-src/README-install.txt || errexit "Cannot tar up the files"    echo "Created blockbuster-install-mac-v$version.tgz containing blocbuster.dmg and sidecar.dmg in current directory." 
 else
     errexit "Error: Unrecognized uname results: $(uname).  Nothing done."
 fi
