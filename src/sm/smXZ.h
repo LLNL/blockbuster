e56804f6dab41d52201273542ebba6285ff73f42
Richard Cook <wealthychef@gmail.com>
2012-10-16 22:58:52 +0000

    add XZ encoder/decoder

---
+++ src/sm/smXZ.h
@@ -0,0 +1,74 @@
+/*
+** $RCSfile: smXZ.h,v $
+** $Name:  $
+**
+** ASCI Visualization Project 
+**
+** Lawrence Livermore National Laboratory
+** Information Management and Graphics Group
+** P.O. Box 808, Mail Stop L-561
+** Livermore, CA 94551-0808
+**
+** For information about this project see:
+** 	http://www.llnl.gov/sccd/lc/img/
+**
+**      or contact: asciviz@llnl.gov
+**
+** For copyright and disclaimer information see:
+**      $(ASCIVIS_ROOT)/copyright_notice_1.txt
+**
+** 	or man llnl_copyright
+**
+** $Id: smXZ.h,v 1.4 2009/05/12 17:25:14 wealthychef Exp $
+**
+*/
+/*
+**
+**  Abstract:
+**
+**  Author:
+**
+*/
+
+
+#ifndef _SM_XZ_H
+#define _SM_XZ_H
+
+//
+// smXZ.h - class for "streamed gzip movies"
+//
+//
+//
+//
+
+#include <sys/types.h>
+
+#ifndef WIN32
+#include <unistd.h>
+#endif
+
+#include "smBase.h"
+
+class smXZ : public smBase {
+   public:
+      smXZ(const char *fname, int _nwin=1);
+      virtual ~smXZ();
+
+      static smXZ *newFile(const char *fname, u_int w, u_int h, u_int nframes,
+		      u_int *tilesizes = NULL, u_int nres=1, 
+                           int numthreads=1);
+
+      static void init(void);
+
+      int getType(void) { return typeID; }
+
+   protected:
+      static smBase *create(const char *_fname, int _nwin);
+      void compBlock(void *in, void *out, int&outsize, int *dim);
+      bool decompBlock(u_char *in,u_char *out,int size, int *dim);
+
+   private:
+      static u_int typeID;
+};
+
+#endif
