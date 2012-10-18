17fd67e9eb18cfe541e89dbfb1d9b3fcee58d82e
Richard Cook <wealthychef@gmail.com>
2012-10-18 23:15:35 +0000

    more robust testing script -- can start expanding

--- blockbuster/tests/img2smtest.py
+++ blockbuster/tests/img2smtest.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import sys, os, shutil, time
+import sys, os, shutil, time, threading
 from subprocess import *
 
 def errexit (msg):
@@ -11,15 +11,43 @@ def usage():
     print "Usage:  img2smtest.py [options]"
     print "Options:"
     print " -bindir dirnam:  path to the directory containing sm2img for disambiguation -- typically this will be $INSTALL_DIR/bin"
-    
-# PARSE ARGUMENTS:
+
 bindir=""
+# PARSE ARGUMENTS:
 if "-bindir" in sys.argv:
     pos = sys.argv.index('-bindir')
     if len(sys.argv) < pos+2:
         usage()
-        sys.exit(-1)        
-    bindir=sys.argv[pos+1] + "/" # make sure there's a trailing '/'
+        sys.exit(-1)
+    bindir=sys.argv[pos+1]
+
+# FIND IMG2SM
+img2sm = "img2sm"
+if bindir != "":
+    img2sm = bindir + '/img2sm'
+    
+try:
+    p = Popen("which %s"%img2sm, shell=True, stdout=PIPE, stderr=STDOUT)
+    p.wait()
+    img2sm = p.stdout.read().strip()
+except:
+    print "Error:  could not find img2sm in PATH or bindir"
+    raise
+
+sys.stderr.write( "Found img2sm at %s"% img2sm)
+# SET UP BINDIR
+if bindir == "":
+    bindir = os.path.abspath(sys.argv[0])
+if bindir[-1] != '/':
+    bindir = bindir + "/" 
+if bindir[0] != '/':
+    bindir = os.getcwd() + '/' + bindir
+
+sys.stderr.write( "bindir is: "+bindir)
+
+# FIND DATA DIR
+datadir = os.path.abspath(os.path.dirname(sys.argv[0])+'/../sample-data')
+sys.stderr.write( "datadir is "+ datadir)
 
 # RUN TESTS:
 testdir = "/tmp/"+os.getenv("USER")+"/img2smtest/"
@@ -28,41 +56,60 @@ os.makedirs(testdir)
 if not os.path.exists(testdir):
     errexit("Cannot create test output directory "+testdir)
 
-def testrun(command, filename):
-    print "Running test:  %s %s"%(command,filename)
-    p = Popen("%s %s"%(command,filename), shell=True, stderr=PIPE, stdout=PIPE)
-
-    secs = 0.0
-    while p.poll() and secs < 15:
-        time.sleep(0.20)
-        secs = secs + 0.2
-
-    if p.poll():
-        print "ERROR: Command %s %s failed to exit within 30 seconds!"%(command,filename)
-        p.terminate()
-        sys.exit(1)
-
-    print "stdout: ", p.stdout.read()
-    print "stderr: ", p.stderr.read()
-    if not os.path.exists(filename):
-        print "ERROR: File %s was not created as expected."%filename
-        sys.exit(2)
-
-    if Popen("sminfo %s"%filename, shell=True).wait():
-        print "ERROR:  sminfo does not like the file %s"%filename
-        sys.exit(3)
-        
-    print "Success!" 
+proc = None
+    
+def runcmd(cmd,outfile):
+    global proc
+    cmd="%s %s %s"%(img2sm, cmd,outfile) 
+    sys.stderr.write( "Running test: \"%s\\n"%cmd)
+    sys.stderr.write("lengh of command: %d\n"%len(cmd))
+    proc = Popen(cmd.split(), bufsize=-1)
+    proc.wait()
     return
+    
+def testrun(cmd,outfile, timeout=5):
+    global success, errmsg, proc
+    theThread = threading.Thread(target=runcmd,args=(cmd,outfile))
+    success=False
+    theThread.start()
+    sys.stderr.write("Waiting for thread to finish...\n")
+    theThread.join(timeout)
 
-testrun(bindir +"img2sm -ignore -form tiff  sample-data/mountains.tiff", "%s/mountains-ignore.sm"%testdir)
-testrun(bindir +"img2sm  -form png -first 084 -last 084 sample-data/quicksand-short-6fps/quicksand-short-6fps%03d.png", "%s/quicksand-single-template.sm"%testdir)
-testrun(bindir +"img2sm -form png -gz sample-data/quicksand-short-6fps/quicksand-short-6fps\%03d.png", "%s/quicksand-all-template-gz.sm"%testdir)
+    
+    if theThread.isAlive():  
+        sys.stderr.write( "ERROR: Command failed to exit within %d seconds!\n"%timeout)
+        os.kill(proc.pid,9)
+        errmsg = "Timeout"
+        
+    elif proc.returncode and proc.returncode < 0:
+        errmsg = "Command returned exit code %d."%proc.returncode
+
+    elif not os.path.exists(outfile):
+        errmsg = "File %s was not created as expected.\n"%outfile 
+
+    elif outfile[-3:] == ".sm":
+        if  Popen("sminfo %s"%outfile, shell=True).wait():
+            errmsg = "sminfo does not like the file %s"%outfile
+        else:
+            success = True            
+    else:
+        success=True
+     
+    
+    sys.stderr.write("\n************************************************\n" )
+    if success:
+        sys.stderr.write("\nSuccess!\n" )
+    else:
+        sys.stderr.write("Failed.  Return code %s, reason: %s\n"%(str(retval),str(errmsg)))
+    sys.stderr.write("\n************************************************\n\n" )
+    return success
+
+
+testrun("-v -ignore -form tiff  %s/mountains.tiff"%datadir, "%s/mountains-ignore.sm"%testdir)
+testrun("-v -form png -first 084 -last 084 %s/quicksand-short-6fps/quicksand-short-6fps%%03d.png "%datadir, "%s/quicksand-single-template.sm"%testdir)
+testrun("-v -form png -gz -first 20 -last 30 %s/quicksand-short-6fps/quicksand-short-6fps%%03d.png "%datadir, "%s/quicksand-all-template-gz.sm"%testdir)
+testrun("-v -form png -lzma -first 20 -last 30 %s/quicksand-short-6fps/quicksand-short-6fps%%03d.png "%datadir, "%s/quicksand-all-template-lzma.sm"%testdir)
 
 sys.exit(0)
-
-#img2sm -ignore -form  svel_clip_geom_rear_hi_res.jpg /nfs/tmp2/rcook/img2smtest/${version}-svel-ignore.sm || errexit "Failed -ignore with version=$version"
-#img2sm -version $version -form png -first 084 -last 084 ~/dataAndImages/langerframes/frame%03d.png /nfs/tmp2/rcook/img2smtest/${version}-langer-single-with-template.sm || errexit "Failed single-with-template with version=$version"
-#img2sm -version $version -form png ~/dataAndImages/langerframes/frame%03d.png /nfs/tmp2/rcook/img2smtest/${version}-langer-multiple-with-template.sm || errexit "Failed multiple-with-template with version=$version"
 
 
