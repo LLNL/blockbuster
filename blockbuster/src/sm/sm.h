2f9f250ff8e10b90e6e44d42417234aa271e1792
Richard Cook <wealthychef@gmail.com>
2013-01-18 18:20:04 +0000

    add sm.h as convenience header

---
+++ blockbuster/src/sm/sm.h
@@ -0,0 +1,13 @@
+#ifndef SM_SM_H
+#define SM_SM_H
+
+
+#include "sm/smLZO.h"
+#include "sm/smRLE.h"
+#include "sm/smGZ.h"
+#include "sm/smXZ.h"
+#include "sm/smRaw.h"
+#include "sm/smJPG.h"
+
+
+#endif
