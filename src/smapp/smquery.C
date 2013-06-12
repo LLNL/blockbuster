a27449e6844d4689e86d310d5143647780594ee8
Richard Cook <wealthychef@gmail.com>
2013-06-12 19:01:38 +0000

    finally get tagging of strings to work properly

--- src/smapp/smquery.C
+++ src/smapp/smquery.C
@@ -20,25 +20,6 @@ void usage(TCLAP::CmdLine  &cmd, char *prog) {
 }
 
 //===================================================================
-string MetaDataValueAsString(SM_MetaData &smdata) {
-  string value ; 
-  if (smdata.mType == METADATA_TYPE_ASCII) {
-    return smdata.mAscii; 
-  }
-  else if (smdata.mType == METADATA_TYPE_INT64) {
-    return str(boost::format("%1%")%smdata.mInt64); 
-  }
-  else if (smdata.mType == METADATA_TYPE_DOUBLE) {
-    return str(boost::format("%1%")%smdata.mDouble); 
-  }
-
-  dbprintf(0, str(boost::format("MatchValue() error: unknown meta data type %1%\n")%smdata.mType).c_str()); 
-  exit(1); 
-
-  return value; 
-}
-
-//===================================================================
 bool MatchesAPattern(const vector<boost::regex> &patterns, string &s) { 
   dbprintf(5, "MatchesAPattern: %d patterns to check\n", patterns.size()); 
 
@@ -69,7 +50,7 @@ int main(int argc, char *argv[]) {
 
   TCLAP::MultiArg<string> valuePatternStrings("V", "Value", "Regexp pattern to match the name the tag being queried", false, "regexp", cmd); 
 
-  TCLAP::SwitchArg thumbnailNum("n", "thumbnail-info", "get number of thumbnail and resolution", cmd); 
+  TCLAP::SwitchArg thumbnailInfo("n", "thumbnail-info", "get number of thumbnail and resolution", cmd); 
 
   TCLAP::SwitchArg extractThumb("e", "extract-thumbnail", "extract thumbnail frame", cmd); 
 
@@ -83,20 +64,27 @@ int main(int argc, char *argv[]) {
 	return 1;
   }
   
-  if (!thumbnailNum.getValue() && !extractThumb.getValue() && !tagPatternStrings.getValue().size() && !valuePatternStrings.getValue().size()) {
+  if (!thumbnailInfo.getValue() && !extractThumb.getValue() && !tagPatternStrings.getValue().size() && !valuePatternStrings.getValue().size() && !matchAll.getValue()) {
+    cerr << "*************************************************" << endl; 
     cerr << "ERROR: You must provide either the --Tag (-T), --Value (-V), --thumbnail-info (-n), or --extract-thumbnail (-e) option." << endl; 
+    cerr << "*************************************************" << endl; 
     usage(cmd, argv[0]); 
     exit(1); 
   }
 
   vector<boost::regex> tagPatterns, valuePatterns; 
-  vector<string> patternStrings = tagPatternStrings.getValue(); 
-  for (uint patno = 0; patno < patternStrings.size(); patno++) {
-    tagPatterns.push_back(boost::regex(patternStrings[patno])); 
-  }
-  patternStrings = valuePatternStrings.getValue(); 
-  for (uint patno = 0; patno < patternStrings.size(); patno++) {
-    valuePatterns.push_back(boost::regex(patternStrings[patno])); 
+  if (matchAll.getValue()) {
+    tagPatterns.push_back(boost::regex(".*")); 
+    valuePatterns.push_back(boost::regex(".*")); 
+  } else {
+    vector<string> patternStrings = tagPatternStrings.getValue(); 
+    for (uint patno = 0; patno < patternStrings.size(); patno++) {
+      tagPatterns.push_back(boost::regex(patternStrings[patno])); 
+    }
+    patternStrings = valuePatternStrings.getValue(); 
+    for (uint patno = 0; patno < patternStrings.size(); patno++) {
+      valuePatterns.push_back(boost::regex(patternStrings[patno])); 
+    }
   }
   
   smBase::init();
@@ -107,9 +95,11 @@ int main(int argc, char *argv[]) {
     string filename = movienames.getValue()[fileno]; 
     smBase *sm = smBase::openFile(filename.c_str(), 1);
     dbprintf(3, "Metadata for %s: (%d entries)\n", filename.c_str(), sm->mMetaData.size()); 
+    int32_t thumbnum = -1, thumbres = -1;
+    
     for (vector <SM_MetaData>::iterator pos = sm->mMetaData.begin();
          pos != sm->mMetaData.end(); pos++) {
-      string mdtag = pos->mTag, mdvalue = MetaDataValueAsString(*pos); 
+      string mdtag = pos->mTag, mdvalue = pos->ValueAsString(); 
       bool tagmatch = MatchesAPattern(tagPatterns, mdtag), 
         valuematch = MatchesAPattern(valuePatterns, mdvalue); 
       if (filenameOnly.getValue()) {
@@ -127,8 +117,24 @@ int main(int argc, char *argv[]) {
           cout << str(boost::format("%1%: Value Match: %2%") % filename % mdvalue) << endl;
         }
       }
+      if (thumbnailInfo.getValue()) {
+        if (mdtag == "SM__thumbframe") {
+          thumbnum = pos->mInt64; 
+          dbprintf(5, "Found thumbnail frame %d\n", thumbnum); 
+       }
+        else if (mdtag == "SM__thumbres") {
+          thumbres = pos->mInt64; 
+          dbprintf(5, "Found thumbnail res %d\n", thumbres); 
+        }
+      }
     }
-    dbprintf(1, str(boost::format("Finished checking %1%") % filename).c_str()); 
+    if (thumbnailInfo.getValue()) {
+      string res = "UNSET", frame = "UNSET"; 
+      if (thumbnum != -1) frame = str(boost::format("%1%")%thumbnum); 
+      if (thumbres != -1) res = str(boost::format("%1%")%thumbres); 
+      cout << str(boost::format("%1%: thumbnail frame: %2%, res: %3%\n") % filename % frame % res) << endl; 
+    }
+    dbprintf(1, str(boost::format("Finished with movie %1%") % filename).c_str()); 
     delete sm;
   }
   
