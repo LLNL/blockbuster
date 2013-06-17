55f479bae179ffef0e3bc5ca84ea71190313db26
Richard Cook <wealthychef@gmail.com>
2013-06-17 21:49:21 +0000

    cleaning

--- tests/smtag_test.py
+++ tests/smtag_test.py
@@ -8,14 +8,5 @@ parser.add_argument('-b', '--bindir', help="set directory where smtag lives", de
 
 args = parser.parse_args()
 
-fixed = test_common.CheckAndFixDir(args.bindir)
-if not fixed:
-    errexit("bindir %s does not exist.  Please use the --bindir argument."%bindir)
-bindir = fixed
-
-smtag = test_common.FindBinary(bindir, "smtag")
-
-bindir = os.path.abspath(os.path.dirname(smtag))
-sys.stderr.write( "bindir is: %s\n"%bindir)
-sys.stderr.write( "Found smtag at %s\n"% smtag)
+[bindir,img2sm,datadir] = test_common.FindPaths(args.bindir, "smtag")
 
