8d994d6381dcff7e3c74c7e9a3419fd0f5156bdb
Richard Cook <wealthychef@gmail.com>
2013-06-18 19:34:50 +0000

    modularize tag code in smapp

--- src/smapp/smtag.C
+++ src/smapp/smtag.C
@@ -40,109 +40,11 @@
 #include "sm/sm.h"
 #include "debugutil.h"
 #include "tags.h"
-#include <stdio.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <map>
+#include <map> 
 typedef boost::tokenizer<boost::char_separator<char> >  tokenizer;
 
 
 using namespace std; 
-
-#define APPLY_ALL_TAG "Apply To All Movies [y/n]?"
-#define USE_TEMPLATE_TAG "Use As Template [y/n]?"
-
-// =====================================================================
-void  GetTagsFromFile(string tagfile, map<string,string> &tagvec){ 
-  dbprintf(0, "Tagfiles are not yet supported. :-( \n"); 
-  exit(1); 
-  return; 
-}
-
-// =====================================================================
-string ResponseSummary(vector<string> &tags, vector<string> &values) {
-  string summary = "SUMMARY OF RESPONSES\n";
-  for (uint num = 0; num < tags.size(); num++) {
-    string value = values[num]; 
-    if (value == "") {
-      value = "(no response yet)"; 
-    }
-    summary += str(boost::format("%1$2d) %2$-33s: current value = \"%3%\"\n") % num % tags[num] % values[num]); 
-  }
-  return summary;   
-}
-
-// =====================================================================
-void GetCanonicalTagValuesFromUser(map<string,string> &canonicals) {
-  vector<string> tags = GetCanonicalTagList(), values; 
-  tags.push_back(APPLY_ALL_TAG); 
-  tags.push_back(USE_TEMPLATE_TAG); 
-  values.resize(tags.size()); 
-  values[tags.size()-2] = "no"; 
-  values[tags.size()-1] = "no"; 
-
-  // synchronize tags/values and canonicals to start up
-  if (!canonicals.size() || canonicals[USE_TEMPLATE_TAG] == "no") {
-    for (uint i = 0; i<tags.size(); i++) {
-       canonicals[tags[i]] = values[i]; 
-    }
-  } 
-  for (uint i = 0; i<tags.size(); i++) {
-    values[i] = canonicals[tags[i]]; 
-  }
-  
-
-  cout << "You will now be asked to supply values for the " << tags.size() << " 'canonical' tags.  At any time, you can enter 'e' or 'exit' to stop the input for this movie without saving your values, 's' or 'save' to stop the input and save your changes, 'm' or 'menu' to be presented with a menu, a number to choose a different tag to enter." << endl;
-  cout << ResponseSummary(tags, values) << endl; 
-
-  string response; 
-  int tagno = 0; 
-  while (true) {
-    if (response == "e" || response == "exit") {
-      cout << "Exiting without saving changes." << endl; 
-      return ; 
-    }
-    else if (response == "s" || response == "save" || tagno == tags.size()) {
-      cout << "Exiting and saving." << endl; 
-      for (uint i = 0; i<tags.size(); i++) {
-        canonicals[tags[i]] = values[i]; 
-      }
-      return ; 
-    } 
-    else if (response == "m" || response == "map") {
-      cout << ResponseSummary(tags, values) << endl; 
-      response = readline("Please enter a key number from the list (-1 to just continue): ");       
-      int rval = -1; 
-      if (response != "" && response != "-1") {
-        try {
-          rval = boost::lexical_cast<short>(response);
-          dbprintf(5, "Got good user response %d\n", rval); 
-        }
-        catch(boost::bad_lexical_cast &) {
-          dbprintf(5, "Got bad user response\n"); 
-          rval = -1; 
-        }
-        if (rval < 0 || rval >= tags.size()) {
-          cout << "Invalid value. "; 
-        } else {
-          tagno = rval; 
-        }
-      }
-    }
-    else if (response != "") {
-      if (boost::regex_match(response, boost::regex("[yY]e*s*"))) response = "yes"; 
-      if (boost::regex_match(response, boost::regex("[Nn]o*"))) response = "no"; 
-      values[tagno] = response; 
-      tagno++; 
-    } 
-    else if (values[tagno] != "") {
-      tagno++; 
-    }
-    response = readline(str(boost::format("Please enter a value for key %1% (default: \"%2%\"): ")%tags[tagno]%values[tagno]).c_str()); 
-  }
-  
-  return; 
-}
 
 // =====================================================================
 void usage(TCLAP::CmdLine &cmd, char *argv0, string msg="") {
@@ -324,7 +226,7 @@ int main(int argc, char *argv[]) {
             values.push_back(pos->second); 
           }
         }
-        cout << ResponseSummary(tags, values) << endl; 
+        cout << TagSummary(tags, values) << endl; 
       }    
       sm->WriteMetaData(); 
       sm->closeFile(); 
@@ -349,7 +251,7 @@ int main(int argc, char *argv[]) {
         values.push_back(pos->second); 
       }
     }
-    cout << ResponseSummary(tags, values) << endl; 
+    cout << TagSummary(tags, values) << endl; 
   }
   
   return 0;
--- src/smapp/tags.h
+++ src/smapp/tags.h
@@ -2,29 +2,24 @@
 #define SM_TAGS_H 1
 #include <vector> 
 #include <string> 
+#include <map>
+using std::map; 
+using std::string;
+using std::vector; 
+
+
+#define APPLY_ALL_TAG "Apply To All Movies [y/n]?"
+#define USE_TEMPLATE_TAG "Use As Template [y/n]?"
+
 /*! 
 	The idea of canonical tags is used by smquery and smtag to 
     give a common context for movies on LC machines
 */ 
-static vector<string> GetCanonicalTagList(void) {
-  vector<string> tags; 
-  tags.push_back("Title"); 
-  tags.push_back("Authors"); 
-  tags.push_back("Description"); 
-  tags.push_back("Science Type"); 
-  tags.push_back("UCRL"); 
-  tags.push_back("Code Name"); 
-  tags.push_back("Sim Start Time"); 
-  tags.push_back("Sim Duration"); 
-  tags.push_back("Sim CPUs"); 
-  tags.push_back("Sim Cluster"); 
-  tags.push_back("Keywords"); 
-  tags.push_back("Movie Creator"); 
-  tags.push_back("Movie Create Date"); 
-  tags.push_back("Movie Create Host"); 
-  tags.push_back("Movie Create Command"); 
-  return tags; 
-}
+vector<string> GetCanonicalTagList(void);
+void GetCanonicalTagValuesFromUser(map<string,string> &canonicals);
+void  GetTagsFromFile(string tagfile, map<string,string> &tagvec);
+void  WriteTagsToFile(string tagfile, map<string,string> &tagvec);
+string TagSummary(vector<string> &tags, vector<string> &values);
 
 #endif
 
