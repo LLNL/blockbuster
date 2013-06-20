eac0c3b795a3bcee15f99cf3e888328cda7062d3
Richard Cook <wealthychef@gmail.com>
2013-06-20 17:26:31 +0000

    eliminating tags.{C,h}

--- blockbuster/src/sm/tags.C
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <map>
-#include "version.h"
-#include "tags.h"
-#include "sm.h"
-typedef boost::tokenizer<boost::char_separator<char> >  tokenizer;
-
-using namespace std; 
