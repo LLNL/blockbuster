32821d5bae721fb76d51d3fd115fecdba4385122
Richard Cook <wealthychef@gmail.com>
2013-06-25 23:50:20 +0000

    working tests again

--- tests/img2smtest.py
+++ tests/img2smtest.py
@@ -16,46 +16,58 @@ test_common.SetBaseDir(basedir)
 # ============================================================================================
 # DEFINE TESTS
 IMG2SM_SUCCESS = "img2sm completed successfully."
+IMG2SM_FAILURE = "ERROR"
+SMQUERY_SUCCESS = None
+SMQUERY_FAILURE = None
+
 tests = [ {"name": "mountains-single",
            "need_data": "mountains.tiff", 
            "cmd": "%s -v mountains.tiff %s"%(img2sm, "mountains.sm"),
            "output": "mountains.sm",
-           "expect_output": IMG2SM_SUCCESS},
+           "failure_pattern": IMG2SM_FAILURE,
+           "success_pattern": IMG2SM_SUCCESS},
           {"name": "check-mountains-single", 
            "need_data": "mountains.sm", 
            'cmd': "%s mountains.sm"%sminfo,
            "output": None,
-           "expect_output": None}, 
+           "failure_pattern": SMQUERY_FAILURE,
+           "success_pattern": SMQUERY_SUCCESS},
           {"name": "quicksand-single-gz",
            "need_data": "quicksand-short-6fps", 
            "cmd": "%s -v --first 084 --last 084 quicksand-short-6fps/quicksand-short-6fps%%03d.png quicksand-single-gz.sm"%img2sm, 
            "output": "quicksand-single-gz.sm",
-           "expect_output":  IMG2SM_SUCCESS}, 
+           "failure_pattern": IMG2SM_FAILURE,
+           "success_pattern":  IMG2SM_SUCCESS}, 
           {"name": "check-quicksand-single-gz", 
            "need_data": "quicksand-single-gz.sm", 
            'cmd': "%s quicksand-single-gz.sm"%sminfo,
            "output": None,
-           "expect_output": None },           
+           "failure_pattern": SMQUERY_FAILURE,
+           "success_pattern": SMQUERY_SUCCESS},           
           {"name": "quicksand-11frames-gz",
            "need_data": "quicksand-short-6fps", 
            "cmd": "%s -v -c gz --first 20 -l 30 quicksand-short-6fps/quicksand-short-6fps%%03d.png quicksand-11frames-gz.sm"%img2sm, 
            "output": "quicksand-11frames-gz.sm",
-           "expect_output": IMG2SM_SUCCESS},           
+           "failure_pattern": IMG2SM_FAILURE,
+           "success_pattern": IMG2SM_SUCCESS},           
           {"name": "check-quicksand-11frames-gz",
            "need_data": "quicksand-11frames-gz.sm", 
            'cmd': "%s quicksand-11frames-gz.sm"%sminfo,
            "output": None,
-           "expect_output": None },           
+           "failure_pattern": SMQUERY_FAILURE,
+           "success_pattern": SMQUERY_SUCCESS},        
           {"name": "quicksand-11frames-lzma",
            "need_data": "quicksand-short-6fps", 
            "cmd": "%s -v --compression lzma --first 20 --last 30 quicksand-short-6fps/quicksand-short-6fps%%03d.png quicksand-11frames-lzma.sm"%img2sm,
            "output": "quicksand-11frames-lzma.sm",
-           "expect_output": IMG2SM_SUCCESS },           
+           "failure_pattern": IMG2SM_FAILURE,
+           "success_pattern": IMG2SM_SUCCESS },           
           {"name": "check-quicksand-11frames-lzma",
            "need_data": "quicksand-11frames-lzma.sm", 
            'cmd': "%s quicksand-11frames-lzma.sm"%sminfo, 
            "output": None,
-           "expect_output": None }         
+           "failure_pattern": SMQUERY_FAILURE,
+           "success_pattern": SMQUERY_SUCCESS}       
          ]
 
 # ============================================================================================
