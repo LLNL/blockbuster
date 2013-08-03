aaa99ce9b17bcd06153bdd93e6300d6b61101833
Richard Cook <wealthychef@gmail.com>
2013-08-03 00:41:36 +0000

    absorb convert.{cpp,h} into their sole clients in frames and x11Renderer

--- blockbuster/src/blockbuster/frames.cpp
+++ blockbuster/src/blockbuster/frames.cpp
@@ -37,7 +37,7 @@
 #include "smFrame.h"
 #include "errmsg.h"
 #include "settings.h"
-#include "convert.h"
+#include <X11/Xlib.h>
 
 static QString matchName; 
 
@@ -47,6 +47,7 @@ static QString matchName;
 #define MYSTAT stat
 #endif
 
+//====================================================================
 /* Note:  __APPLE__ assumes gcc on OS X*/
 #if defined(irix) || defined(aix) || defined(__APPLE__)
 static int matchStartOfName(struct dirent *entry)
@@ -58,6 +59,196 @@ static int matchStartOfName(struct dirent *entry)
   return (QString(entry->d_name) == matchName); 
 }
 
+
+//====================================================================
+void FrameInfo::ConvertPixel(const ImageFormat *srcFormat,
+                         const ImageFormat *destFormat,
+                         const unsigned char *src, unsigned char *dest)
+{
+  unsigned char red, green, blue;
+
+  /* Pull out the R, G, and B components */
+  if (srcFormat->bytesPerPixel >= 3) {
+	if (srcFormat->byteOrder == MSB_FIRST) {
+      red = *src++;
+      green = *src++;
+      blue = *src++;
+	}
+	else {
+      blue = *src++;
+      green = *src++;
+      red = *src++;
+	}
+  }
+  else {
+	/* Fill this in with smarter code later, if necessary */
+	static int forewarned = 0;
+	if (!forewarned) {
+      ERROR("trying to convert from %d bytes per pixel - using black",
+		    srcFormat->bytesPerPixel);
+      forewarned = 1;
+	}
+	red = 0;
+	green = 0;
+	blue = 0;
+  }
+  if (destFormat->bytesPerPixel >= 3) {
+	int destExtraBytes = destFormat->bytesPerPixel - 3;
+	if (destFormat->byteOrder == MSB_FIRST) {
+      *dest++ = red;
+      *dest++ = green;
+      *dest++ = blue;
+	}
+	else {
+      *dest++ = blue;
+      *dest++ = green;
+      *dest++ = red;
+	}
+	while (destExtraBytes-- > 0) {
+      /* This is probably going to be interpreted as alpha or something.
+       * We may need a different constant value, or even a variant value.
+       */
+      *dest++ = 0;
+	}
+  }
+  else if (destFormat->bytesPerPixel == 2) {
+#define SHIFT(a,b) ((b)>0?(a)<<(b):(a)>>-(b))
+	unsigned long pixel = 
+      (SHIFT(red, destFormat->redShift) & destFormat->redMask) |
+      (SHIFT(green, destFormat->greenShift) & destFormat->greenMask) |
+      (SHIFT(blue, destFormat->blueShift) & destFormat->blueMask);
+	if (destFormat->byteOrder == LSB_FIRST) {
+      *dest++ = pixel & 0xff;
+      *dest++ = (pixel >> 8) & 0xff;
+	}
+	else {
+      *dest++ = (pixel >> 8) & 0xff;
+      *dest++ = pixel & 0xff;
+	}
+  }
+  else if (destFormat->bytesPerPixel == 1) {
+	/* maybe make this smarter later */
+	static int forewarned = 0;
+	if (!forewarned) {
+      ERROR("unsuccessful conversion to 1 byte per pixel");
+      forewarned = 1;
+	}
+	*dest++ = 0;
+  }
+  else {
+	static int forewarned = 0;
+	if (!forewarned) {
+      ERROR("trying to convert to %d bytes per pixel?!?", 
+		    destFormat->bytesPerPixel);
+      forewarned = 1;
+	}
+  }
+}
+
+//====================================================================
+ImagePtr FrameInfo::ConvertImageToFormat( ImagePtr image, ImageFormat *canvasFormat)
+{
+  /* We used to check for optimized cases; but any time we're
+   * here, we're unoptimized, so now just do as complete a job
+   * as possible, without worrying about efficiency.
+   */
+  DEBUGMSG("ConvertImageToFormat(frame %d)", image->frameNumber); 
+  const ImageFormat *srcFormat = &image->imageFormat;
+  const ImageFormat *destFormat = canvasFormat;
+
+  const int srcBytesPerPixel = srcFormat->bytesPerPixel;
+  const int destBytesPerPixel = destFormat->bytesPerPixel;
+
+  const int srcBytesPerScanline = ROUND_TO_MULTIPLE(
+                                                    srcBytesPerPixel * image->width,
+                                                    srcFormat->scanlineByteMultiple
+                                                    );
+  const int destBytesPerScanline = ROUND_TO_MULTIPLE(
+                                                     destBytesPerPixel * image->width,
+                                                     destFormat->scanlineByteMultiple
+                                                     );
+  register const unsigned char *srcScanline, *src;
+  register unsigned char *destScanline, *dest;
+  register uint32_t x, y;
+
+  /* Check to make sure that the image isn't good enough already. 
+   * We have to check here, instead of earlier, because it's possible
+   * for two formats with different "bytesPerScanline" values to 
+   * be compatible, if the actual computed bytes per scanline happens
+   * to match.
+   */
+  if (
+      srcBytesPerPixel == destBytesPerPixel &&
+      srcBytesPerScanline == destBytesPerScanline &&
+      srcFormat->byteOrder == destFormat->byteOrder &&
+      (srcFormat->rowOrder == destFormat->rowOrder ||
+       destFormat->rowOrder == ROW_ORDER_DONT_CARE)
+      ) {
+    /* It's possible that we match - we need a closer look. */
+    if (srcBytesPerPixel >= 3) {
+      /* Match, as we ignore the extra bytes in a pixel */
+      return image;
+    }
+      
+    /* Less than 3 bytes per pixel - we match if our
+     * shifts and masks all match.
+     */
+    if (srcFormat->redShift == destFormat->redShift &&
+        srcFormat->greenShift == destFormat->greenShift &&
+        srcFormat->blueShift == destFormat->blueShift &&
+        srcFormat->redMask == destFormat->redMask &&
+        srcFormat->greenMask == destFormat->greenMask &&
+        srcFormat->blueMask == destFormat->blueMask)
+      return image;
+  }
+  /* Otherwise, we suffer a non-trivial conversion. Create a new
+   * image from scratch.
+   */
+  ImagePtr destImage(new Image()); 
+  if (!destImage) {
+	ERROR("could not allocate Image structure");
+	return ImagePtr();
+  }
+  if (!destImage->allocate(image->height * destBytesPerScanline)) {
+	ERROR("could not allocate %dx%dx%d image data",
+          image->height, image->width, destBytesPerPixel*8);
+	return ImagePtr();
+  }
+
+  if ((srcFormat->rowOrder != destFormat->rowOrder) && (destFormat->rowOrder != ROW_ORDER_DONT_CARE))
+    srcScanline = (unsigned char *) image->ConstData() + (image->height - 1) * srcBytesPerScanline;
+  else
+    srcScanline = (unsigned char *) image->ConstData();
+
+  /* XXX this code does not properly handle the loadedRegion info!!!
+   * It was broken before and just removed for now.
+   */
+  destScanline = (unsigned char *) destImage->Data();
+  for (y = 0; y < image->height; y++) {
+	src = srcScanline;
+	dest = destScanline;
+	for (x = 0; x < image->width; x++) {
+      ConvertPixel(srcFormat, destFormat, src, dest);
+      src += srcBytesPerPixel;
+      dest += destBytesPerPixel;
+	}
+    if ((srcFormat->rowOrder != destFormat->rowOrder) && (destFormat->rowOrder != ROW_ORDER_DONT_CARE))
+      srcScanline -= srcBytesPerScanline;
+    else
+      srcScanline += srcBytesPerScanline;
+	destScanline += destBytesPerScanline;
+  }
+
+  destImage->width = image->width;
+  destImage->height = image->height;
+  destImage->imageFormat = *canvasFormat;
+  if (canvasFormat->rowOrder == ROW_ORDER_DONT_CARE)
+    destImage->imageFormat.rowOrder = image->imageFormat.rowOrder;
+  bb_assert(destImage->imageFormat.rowOrder != ROW_ORDER_DONT_CARE);
+  destImage->loadedRegion = image->loadedRegion;
+  DEBUGMSG("Done with ConvertImageToFormat"); 
+  return destImage;
+}
 
 //===============================================================
 /* This utility function handles loading and converting images appropriately,
--- blockbuster/src/blockbuster/x11Renderer.h
+++ blockbuster/src/blockbuster/x11Renderer.h
@@ -22,7 +22,10 @@ class x11Renderer: public Renderer {
     return NULL; // does not apply to X11
   }
  
-  virtual void Render(int frameNumber,
+ ImagePtr ScaleImage(ImagePtr image, 
+                     int srcX, int srcY, int srcWidth, int srcHeight,
+                     int zoomedWidth, int zoomedHeight);
+ virtual void Render(int frameNumber,
               const Rectangle *imageRegion,
               int destX, int destY, float zoom, int lod);
   virtual void DrawString(int row, int column, const char *str);
