1b32cf8d90cda3330d38d08ec0a381dee25084e4
Richard Cook <wealthychef@gmail.com>
2013-09-20 23:07:10 +0000

    add support for building/not building/linking boost as appropriate for circumstances on each machine.

---
+++ blockbuster/src/boost/findboost.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -xv 
+
+if [ "$NOBOOSTLINK" != "YES" ]; then 
+    for prefix in $BOOST_DIR /usr/local/tools /usr/local/ /opt/local; do
+        for boost in boost ""; do 
+            for infix in -nompi -mpi ""; do 
+                for version in -1.54.0 -1.53.0 -1.49.0 ""; do 
+                    dir=$prefix/${boost}${infix}${version}
+                    if ls $dir/lib/libboost_thread.a  $dir/lib/libboost_system.a  $dir/lib/libboost_regex.a $dir/lib/libboost_date_time.a $dir/include/boost/random.hpp>/dev/null 2>&1; then 
+                        if [ "$dir" != "$INSTALL_DIR" ]; then
+                            rm -rf $INSTALL_DIR/include/boost $INSTALL_DIR/lib/libboost_*
+                            ln -s $dir/include/boost $INSTALL_DIR/include/boost 
+                            for lib in $dir/lib/libboost_{date_time,regex,system,thread}*; do 
+                                ln -s $lib $INSTALL_DIR/lib/
+                            done
+                        fi
+                        echo $dir
+                        exit 0
+                    fi
+                done
+            done
+        done
+    done
+fi
+rm -rf $INSTALL_DIR/include/boost $INSTALL_DIR/lib/libboost_*
+export INSTALL_DIR=$INSTALL_DIR
+make -e build_boost 1>&2 || exit 1
+echo $INSTALL_DIR
+exit 0
