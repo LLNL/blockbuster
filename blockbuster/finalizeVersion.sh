ecc33148e0db85800b7805fd128a52645cb78e2b
Rich Cook <rcook@llnl.gov>
2014-02-26 18:07:50 -0800

    add auk to finalizeVersion.sh script
    Former-commit-id: 5c4d7345e891f5a25ecf3226008a8468622af534

--- blockbuster/finalizeVersion.sh
+++ blockbuster/finalizeVersion.sh
@@ -7,6 +7,7 @@ function usage() {
     echo "updates version.h "
     echo "greps Changelog to make sure there is an entry there.  Updates the Changelog entry. "
     echo " OPTIONS: " 
+    echo "-a/--auk:  include auk61 on the deployment" 
     echo "-c/--commit: Commit changes to all tracked directories and files before proceeding. Default: only commit version-related files."
     echo "-t/--temp: Just update Changelog and version.h as needed."
     echo "-f/--final: updates version.h and Changelog, creates a tarball in the current directory with proper naming scheme. " 
@@ -76,9 +77,12 @@ fi
 temp=false
 final=false
 commit=false
+aukbuild=false
 # must give either -temp or -final: 
 for arg in "$@"; do 
-    if [ "$arg" == --commit ]  ||  [ "$arg" == -c ] ; then
+    if [ "$arg" == --auk ]  ||  [ "$arg" == -a ] ; then
+        aukbuild=true
+    elif [ "$arg" == --commit ]  ||  [ "$arg" == -c ] ; then
         commit=true
     elif [ "$arg" == --temp ]  ||  [ "$arg" == -t ] ; then
         temp=true
@@ -243,6 +247,11 @@ rm -rf $tmpdir
 # =============================================================
 echo "Installing software..." 
 
+if $aukbuild; then 
+    scp $builddir/blockbuster-v${version}.tgz auk61:/viz/blockbuster/tarballs/
+    ssh auk61 "set -xv; mkdir -p /viz/blockbuster/${version} && pushd /viz/blockbuster/${version} &&  tar -xzf /viz/blockbuster/tarballs/blockbuster-v${version}.tgz && pushd blockbuster-v${version} && INSTALL_DIR=/viz/blockbuster/${version} make && rm /viz/blockbuster/latest && ln -s /viz/blockbuster/${version} latest" || errexit "build on auk failed"
+fi
+
 echo '#!/usr/bin/env bash
 . '"$HOME/.profile"'
 export PATH=/usr/local/tools/qt-4.8.4/bin:$PATH
@@ -287,5 +296,8 @@ ln -s $version test
 popd
 echo "Done.  Tarball is $builddir/blockbuster-v${version}.tgz.  To use the new version, type \"use asciviz-test\""
 ln -s $builddir/blockbuster-v${version}.tgz $installdir/blockbuster-v${version}.tgz
+
+echo "Built and installed blockbuster-v${version} and made it the test version on LC and latest on auk" | mail -s "blockbuster-v${version} build complete" rcook@llnl.gov
+
 exit 0
 
