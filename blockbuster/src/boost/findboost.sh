79e500ad393cd387fc864ae1b23a3e90e551e139
Rich Cook <rcook@llnl.gov>
2014-04-10 14:19:13 -0700

    add program_options to boost
    Former-commit-id: 98524a53418bc5c6d03afd4ddffd4909cfccbc75

--- blockbuster/src/boost/findboost.sh
+++ blockbuster/src/boost/findboost.sh
@@ -7,11 +7,12 @@ if [ "$NOBOOSTLINK" != "YES" ]; then
             for infix in -nompi -mpi ""; do 
                 for version in -1.54.0 -1.53.0 -1.49.0 ""; do 
                     dir=$prefix/${boost}${infix}${version}
-                    if ls $dir/lib/libboost_thread.a $dir/lib/libboost_atomic.a  $dir/lib/libboost_system.a  $dir/lib/libboost_regex.a $dir/lib/libboost_date_time.a $dir/include/boost/random.hpp>/dev/null 2>&1; then 
+                    libs=$dir/lib/libboost_{atomic,date_time,filesystem,regex,system,thread,program_options}*
+                    if ls $libs >/dev/null 2>&1; then 
                         if [ "$dir" != "$INSTALL_DIR" ]; then
                             rm -rf $INSTALL_DIR/include/boost $INSTALL_DIR/lib/libboost_*
                             ln -s $dir/include/boost $INSTALL_DIR/include/boost 
-                            for lib in $dir/lib/libboost_{atomic,date_time,filesystem,regex,system,thread}*; do 
+                            for lib in $libs; do 
                                 ln -s $lib $INSTALL_DIR/lib/
                             done
                         fi
@@ -23,10 +24,11 @@ if [ "$NOBOOSTLINK" != "YES" ]; then
         done
     done
 fi
-if [ ! -d $INSTALL_DIR/include/boost ] || [ ! -f $INSTALL_DIR/lib/libboost_thread.a ]; then
-    rm -rf $INSTALL_DIR/include/boost $INSTALL_DIR/lib/libboost_*
-fi
-export INSTALL_DIR=$INSTALL_DIR
-make -e build_boost 1>&2 || exit 1
-echo $INSTALL_DIR
+for thing in $INSTALL_DIR/lib/libboost_{atomic,date_time,filesystem,regex,system,thread,program_options}* $INSTALL_DIR/include/boost; do 
+    if [ ! -f $thing ]; then
+        rm -rf $INSTALL_DIR/include/boost $INSTALL_DIR/lib/libboost_*
+        break
+    fi
+done
+INSTALL_DIR=$INSTALL_DIR  make -e build_boost 1>&2 || exit 1
 exit 0
