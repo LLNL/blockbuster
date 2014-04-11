0d83723046981030c6a9ab3350c4a0b9c4566bc6
Rich Cook <rcook@llnl.gov>
2014-04-10 18:23:10 -0700

    move Prefs.{C,h} up to blockbuster directory to allow usage by bb and sidecar
    Former-commit-id: 398c46f0303e1df27e35e77c75c4960ea1928b93

---
+++ blockbuster/src/blockbuster/Prefs.C
@@ -0,0 +1,441 @@
+#define NO_BOOST 1
+#include "Prefs.h"
+#include "stringutil.h"
+#include "errno.h"
+/* class Preferences
+   member functions 
+*/
+// $Revision: 1.27 $
+
+//#ifdef __USE_GNU
+extern char **environ;
+
+//#endif
+
+using namespace std;
+
+// IF YOU WANT TO DEBUG PREFS, set the "debugprefs" environment variable to 1
+static string debugprefs("false"); 
+#define prefsdebug if (debugprefs != "false") cerr << __FILE__ <<" line "<<__LINE__ << ": "
+
+//====================================================
+void Preferences::Merge(const Preferences &other, bool addnew, bool overwrite) {
+
+  if (!addnew && !overwrite) return; // one must be true for anything to happen
+
+  std::map<string, string>::const_iterator pos = other.mPrefs.begin(), endpos = other.mPrefs.end(), found; 
+  while (pos != endpos) {
+    found = mPrefs.find(pos->first); 
+    if ((addnew && found == mPrefs.end()) ||
+        (overwrite && found != mPrefs.end()) ) {
+      mPrefs[pos->first] = pos->second; 
+    }
+    ++pos; 
+  }
+}
+      
+
+//====================================================
+void Preferences::Reset(void){
+  ClearPrefs();
+  mFilename  = "";
+
+  char *dbptr = getenv("debugprefs");
+  if (dbptr) {
+    debugprefs = dbptr;
+    cerr << "enabling debugging of prefs because debugprefs environment variable is set" << endl; 
+  }
+ 
+  SetLabel("SoleRecord");//if "SoleRecord", then we know there is only one section in the file
+  _dirty = 0;
+  _writtenToDisk = 0;
+  return;
+}
+
+//====================================================
+bool Preferences::TryGetValue(const string &key,  std::string &outValue) const {
+  outValue = GetValue(key, false);    
+  if (outValue == "") return false; 
+  return true; 
+}
+
+//====================================================
+bool Preferences::TryGetDoubleValue(const string &key, double &outValue) const {
+  outValue = GetDoubleValue(key, false); 
+  if (outValue == DOUBLE_FALSE) { 
+    return false; 
+  }
+  return true; 
+}
+
+//====================================================
+bool Preferences::TryGetLongValue(const string &key, long &outValue) const {
+  outValue = GetLongValue(key, false);   
+  if (outValue == LONG_FALSE) {
+    return false; 
+  }
+  return true; 
+}
+
+//====================================================
+std::string Preferences::GetValue(const std::string &key, bool dothrow) const { 
+  if (mPrefs.find(key) == mPrefs.end()) {
+    if (dothrow) {
+      throw string(key+" not found");
+    } else {
+      return string("");
+    }
+  }
+  return mPrefs.find(key)->second;
+}
+
+//====================================================
+double Preferences::GetDoubleValue(const string &key, bool dothrow) const {
+  char *endptr;
+  string value = GetValue(key, dothrow);
+  double result = strtod(value.c_str(), &endptr);
+  if (endptr == value.c_str()) {
+    if (dothrow) {
+      throw string("Bad key in  Preferences::GetDoubleValue");
+    } else {
+      return DOUBLE_FALSE;
+    }
+  }
+  return result;  
+}
+
+//====================================================
+long Preferences::GetLongValue(const string &key, bool dothrow) const {
+  char *endptr;
+  string value = GetValue(key, dothrow);
+  long result = strtol(value.c_str(), &endptr, 10);
+  if (endptr == value.c_str()){
+    if (dothrow) {
+      throw string("Bad key in  Preferences::GetDoubleValue");
+    } else {
+      return LONG_FALSE;
+    }
+ }
+  
+  return result;  
+}
+
+
+//====================================================
+void Preferences::SetValue(const string &key, double value){
+  SetValue(key, doubleToString(value));
+  return;
+}
+
+//====================================================
+/* Look in the environment for new values for previously declared keys */ 
+void Preferences::ReadFromEnvironment(void){
+  prefsdebug << "ReadFromEnvironment"<<endl;
+  char **envp = environ; 
+  char *currentstring; 
+  while ((currentstring = *envp) != NULL) {
+    string keypair(currentstring); 
+    string::size_type equals = keypair.find('=');
+    if (equals == string::npos) {
+      throw string("Error in environment:  environment string is malformed: \"") + keypair + "\""; 
+    }    
+    string key = keypair.substr(0, equals), value = keypair.substr(equals+1);
+    //if (mPrefs.find(key) != mPrefs.end()) {
+    prefsdebug << "Setting prefs key "<< key<< " to value from environment: " << value << endl; 
+    mPrefs[key] = value;
+    //}
+    ++envp; 
+  }
+  return; 
+}
+
+//====================================================
+/* skip comments and get the next valid token */
+string Preferences::NextKey(ifstream&theFile){
+  char buf[2048];
+  string key("");
+  prefsdebug << "Preferences::NextKey"<<endl; 
+  try {
+    while (theFile) {
+      theFile >> key;     
+      if (key.length() && key[0] == '#')     
+	theFile.getline(buf, 2048);
+      else
+	break;
+    }
+  } catch (...) {
+    cerr << "Error in NextKey()" << endl; 
+    throw; 
+  }
+  return key;
+}
+
+//====================================================
+map<string, string> Preferences::ReadNextSection(ifstream &theFile){
+  map<string, string> theMap;
+  char buf[2048];
+  string key, value; 
+  prefsdebug<<"Preferences::ReadNextSection "<<endl;
+  try { 
+    key = NextKey(theFile);
+  } catch (string err ) {
+    cerr << "Error getting first key in prefs"<<endl;
+    throw err;
+  }   if (!key.size())
+    throw string("Unexpected EOF in prefs");
+
+  if (key != "BEGINRECORD")
+    throw string("Prefs records must begin with BEGINRECORD, not \"")+key+"\"";
+  prefsdebug << "found BEGINRECORD in prefs file" << endl;
+  fflush(stdout); 
+  theFile.getline(buf, 2048);//toss away rest of line
+
+  char gotlabel = 0;
+  while (key.size()) {
+    try {
+      key = NextKey(theFile);
+    } catch (string err) {
+      cerr << "Error getting next key" << endl; 
+      break; 
+    }
+    prefsdebug << "Found key: " << key << endl; 
+    fflush(stdout); 
+    if (!gotlabel) {
+      if (key != "prefs_label") {
+	// this needs to be the label
+	throw string("Improperly formatted line in file: ")+key  + string(buf);
+      }
+      gotlabel=1;
+    }
+    if (key == "ENDRECORD")
+      break;
+    theFile.getline(buf, 2048); // get the rest of the line
+    string value(buf);
+    if (value == "") {
+      prefsdebug<< "No valid value found for line, skipping..."<<endl;
+      continue; 
+    }
+    string::size_type idx = value.find_first_not_of(" \t\n\r");
+    value = value.substr(idx);
+    theMap[key] =value;    
+    prefsdebug<< "map["<<key<<"] = \""<<value<<"\""<<endl; 
+    fflush(stdout); 
+  }
+
+  if (key != "ENDRECORD")
+    throw string("Cannot find valid map in file.");
+  return theMap;
+}
+
+//====================================================
+ //open the file, seek past label, read my section, close file
+/* this may optionally throw an exception if it can't be read from the file 
+   by default it won't
+ */
+void Preferences::ReadFromFile(bool throw_exceptions){
+  ifstream theFile(mFilename.c_str());
+  prefsdebug<<"Preferences::ReadFromFile"<<endl;
+  if (!theFile) {
+    if (throw_exceptions) {
+      throw string("Can't open file for reading: ")+mFilename;
+    }
+    return; 
+  }
+  map<string, string> current;
+  current["prefs_label"] = "!!!??? nothing at all (no match)";
+  string targetlabel;
+  try {
+    targetlabel=GetLabel(); 
+  } catch (...) {
+    cerr << "GetLabel failed in Preferences::ReadFromFile" << endl; 
+    if (throw_exceptions) {
+      throw; 
+    }
+    return; 
+  }
+  while (current["prefs_label"] != targetlabel){
+    try {
+      prefsdebug << "read next section"<<endl;
+      current = ReadNextSection(theFile);
+    }
+    catch(string s){
+      cerr << "Warning: can't read prefs \""<<GetLabel()<<"\" from file "<<mFilename<<endl;
+      cerr << s << endl;
+      if (throw_exceptions){
+        throw s;
+      }
+      return;
+    }
+    map<string, string>::iterator pos = current.begin(), endpos = current.end(); 
+    while (pos != endpos) {
+      mPrefs[pos->first] = pos->second; 
+      ++pos; 
+    }
+  }
+  prefsdebug << "Finished reading prefs: "<< string(*this) << endl;
+  _dirty = 0;
+  _writtenToDisk = 1;
+  return;
+}
+
+//====================================================
+bool Preferences::KeyValid(string key) {
+  vector<argType>::iterator pos = mValidArgs.begin(), endpos = mValidArgs.end(); 
+  while (pos != endpos) {
+    if (key == pos->mKey) return true; 
+    ++pos; 
+  }
+
+  return false; 
+}
+
+//====================================================
+void Preferences::SaveSectionToFile(ofstream &outfile, map<string, string> &section){
+  outfile <<"BEGINRECORD"<<endl;
+  outfile << "prefs_label\t" << section["prefs_label"]<<endl;
+  map<string, string>::iterator pos = section.begin();
+  while (pos != section.end()){
+    if (KeyValid(pos->first)) {
+      outfile << pos->first << "\t" ;
+      if (!(pos->second).size())
+        outfile << "(nil)"<<endl;
+      else {
+        string clean = pos->second; 
+        replace(clean.begin(), clean.end(), '\n', '\t'); 
+        outfile << clean << endl;
+      }      
+    }
+    ++pos;
+  }
+  outfile << "ENDRECORD" << endl;
+  return; 
+}
+  
+//====================================================
+void Preferences::SaveToFile(bool createDir, bool clobber){
+  vector<map<string, string> > saved;
+  map<string, string> current;  
+  map<string, string>::iterator pos;
+  
+  if (createDir) {
+    string::size_type idx = mFilename.rfind('/'); 
+    if (idx != string::npos) {
+      string dirname = mFilename.substr(0, idx); 
+      system((string("mkdir -p ")+dirname).c_str()); 
+    }
+  }
+  char found = 0;
+  if (!clobber) {
+    cerr << "preserving old pref file" << endl; 
+    ifstream infile(mFilename.c_str()); 
+    while (infile){
+      try {
+        current = ReadNextSection(infile);
+        pos = current.find("prefs_label");
+        if (pos!=current.end() && pos->second == GetLabel()) {
+          found = 1;
+          saved.push_back(mPrefs);
+        }
+        else 
+          saved.push_back(current);
+      }
+      catch(...){
+        break;
+      }
+    }
+  }
+   if (!found)
+      saved.push_back(mPrefs);
+ 
+  vector<map<string, string> >::iterator outpos = saved.begin();
+  if (outpos == saved.end())
+    throw string("Prefs: Fatal Error: nothing to save!");
+  
+  ofstream outfile(mFilename.c_str());
+  if (!outfile)
+    throw string("Cannot open file for writing: ")+mFilename;
+  
+  while (outpos != saved.end()){
+    SaveSectionToFile(outfile, *outpos);
+    ++outpos;
+  }
+  prefsdebug << "Finished writing prefs."<<endl;
+  mPrefs = current;
+  return;
+}
+
+void ConsumeArg(int argnum, int &argc, char *argv[]){
+  while (argnum < argc-1) {
+    argv[argnum] = argv[argnum+1];
+    ++argnum; 
+  }
+  argv[argnum] = NULL; 
+  argc--; 
+}
+void Preferences::GetFromArgs(int &argc, char *argv[], vector<argType>& argtypes) {
+  
+  // first, initialize all keys in "types" array to defaults if not already set to some value
+  vector<argType>::iterator argPos; 
+  for (argPos = argtypes.begin(); argPos != argtypes.end(); ++argPos) {
+    if (mPrefs.find(argPos->mKey) == mPrefs.end()) {      
+      if (argPos->mType == "bool") {
+        SetBoolValue(argPos->mKey, false); 
+      } else if (argPos->mType == "string") {
+        SetValue(argPos->mKey, string(""));
+      } else if (argPos->mType == "long" || argPos->mType == "double") {
+        SetValue(argPos->mKey, 0); 
+      }
+    }
+  }
+       
+  // now, go through and parse the argc and argv as command line options.
+  errno = 0; 
+  int argnum = 0; 
+  string foundflag; 
+  while (argnum < argc) {
+    for (argPos = argtypes.begin();  
+         argPos != argtypes.end(); 
+         ++argPos) {
+      for (vector<string>::iterator flagpos = argPos->mFlags.begin(); 
+           flagpos != argPos->mFlags.end();
+           flagpos++) {
+        if (*flagpos == string(argv[argnum])) {
+          foundflag = *flagpos;   
+          break; 
+        }
+      }
+      if (foundflag != "") break; 
+    }
+
+    if (foundflag == "") {
+      argnum++; 
+    } else {
+       // do not increment argnum here as you will simply be consuming args and reducing argc
+      ConsumeArg(argnum, argc,argv); 
+    
+      if (argPos->mType == "bool") {
+        SetBoolValue(argPos->mKey, true); 
+      } else {
+        if (argnum == argc) {
+          throw string("Flag ")+foundflag+string(" requires an argument");
+        }     
+        if (argPos->mType == "string") {
+          SetValue(argPos->mKey, string(argv[argnum]));
+        } else if (argPos->mType == "long") {
+          SetValue(argPos->mKey, strtol(argv[argnum], NULL, 10));
+          if (errno) {
+            throw string("Could not convert ")+argv[argnum]+string(" to long");
+          }
+        } else if (argPos->mType == "double") {
+          SetValue(argPos->mKey, strtod(argv[argnum], NULL) ); 
+          if (errno) {
+            throw string("Could not convert ")+argv[argnum]+string(" to double");
+          }
+        } else {
+          throw string("Bad keytype: ")+argPos->mType+string(" for key: ")+argPos->mKey; 
+        }
+        ConsumeArg(argnum, argc,argv); 
+      } // end check which arg type it is
+    } /* end  if (found) */
+  }// end while (argnum < argc)
+}
---
+++ blockbuster/src/blockbuster/Prefs.h
@@ -0,0 +1,212 @@
+/* This file is an attempt to allow any application to read its preferences into what Randy Frank would call a "mapobj".  I have stolen his idea and hopefully improved it to be more general and more robust, because it no longer relies on pointers to store its information.  In fact, it no longer allows pointers to be stored at all.  The presumption is that this is non-volatile information which can be written to disk.  I don't know of a way yet to pickle C items.  Maybe later I'll change to a binary output format and then allow any data to be captured to disk.  Not today. 
+   All values are stored as C++ strings.  Functions which set or get values as other types are merely converting a string to the desired type or vice-versa.
+
+   To do:  multiple Preferences should be able to be written to a single file. 
+ */
+// $Revision: 1.20 $
+#ifndef RCPREFS_H
+#define RCPREFS_H
+
+#include <map>
+#include <iostream>
+#include <vector> 
+#include <string>
+#include <fstream>
+#include "stringutil.h"
+#include <boost/format.hpp>
+
+#define DOUBLE_FALSE (0.0)
+#define LONG_FALSE  (0)
+using namespace boost; 
+//======================================================
+struct argType {
+  argType(string key) {
+    argType(key, "string"); 
+  }
+  argType(string key, string preftype) {
+    mKey = key; 
+    mType = preftype; 
+    mFlags.push_back(str(format("--%1%")%key)); 
+    mFlags.push_back(str(format("-%c")%key[0])); 
+  }  
+  argType(vector<string> flags, string key, string preftype) {
+    mKey = key; 
+    mType = preftype; 
+    mFlags = flags;  
+  }
+  argType(string longflag, string shortflag, string key, string preftype) {
+    mKey = key; 
+    mType = preftype; 
+    mFlags.push_back(longflag); 
+    mFlags.push_back(shortflag); 
+  }
+  argType(string flag, string key, string preftype) {
+    mKey = key; 
+    mType = preftype; 
+    mFlags.push_back(flag); 
+  }
+
+  argType(const argType &other) {
+    *this = other; 
+  }
+
+  const argType &operator =(const argType &other) {
+    mKey = other.mKey; 
+    mType = other.mType; 
+    mFlags = other.mFlags; 
+    return *this; 
+  }
+
+  vector<string> mFlags; // aliasable e.g. "--help" and "-h" 
+  string mKey, 
+    mType; // "bool", "long", "double", "string"
+};
+    
+//======================================================
+class Preferences {
+ public:
+  //========================
+  //Initialization
+  Preferences(){Reset();}
+  Preferences(std::string filename) { Reset(); SetFile(filename);}
+  
+  /* Merge:  combine this with other.  
+     Iff addnew is true, then keys in other but not in this will be added to this
+     Iff overwrite is true, then keys in other overwrite any keys in this by the same name.
+     Note that if both are false, then nothing will be done! 
+  */
+  void Merge(const Preferences &other, bool addnew=true, bool overwrite=true); 
+  // conveniences:
+  void MergeNoOverwrite(const Preferences &other) { Merge(other, true, false); }
+  void MergeNoAdd(const Preferences &other) {Merge(other, false, true); }
+
+  void Reset(void); 
+  void ClearPrefs(void) { mPrefs.clear();}
+  std::map<std::string, std::string> GetPrefsMap(void) { return mPrefs; }
+
+  //========================
+  operator string() const {
+    string tmp = string("Preferences: \n")
+      + "mFilename: : " + mFilename + "\n" 
+      +  "mPrefs:\n"; 
+    map<string, string>::const_iterator pos = mPrefs.begin(), end = mPrefs.end();
+    while(pos!=end) {
+      tmp +=  " (" + pos->first + ", " + pos->second + ") \n";
+      ++pos;
+    }
+    tmp +=  str(boost::format("_dirty: %1%\n_writtenToDisk: %2%\n")%_dirty%_writtenToDisk); ;
+    return tmp; 
+  }
+
+  //========================
+  // meta-attributes
+  void SetFile(std::string name){ mFilename = name;}
+  std::string GetFilename(void) { return mFilename;}
+
+  // disambiguate self from other prefs in a file:
+  void SetLabel(std::string label){ SetValue("prefs_label", label);}
+  std::string GetLabel(void) { return GetValue ("prefs_label");}
+
+  //========================
+  //saving and restoring from disk
+  void SaveToFile(bool createDir=true, bool clobber=true); //open the file, read and remember previous sections, read and discard my section, read and remember sections after me, then write previous section, my section, and trailing sections, then close the file.  Optionally create the needed directory for the file. 
+  void SaveToFile(string filename, bool createDir=false) {
+    //convenience function
+    SetFile(filename); 
+    SaveToFile(createDir); 
+  }
+
+  void ReadFromFile(bool throw_exceptions=false); //open the file, seek past label, read my section, close file
+  void ReadFromFile(string filename, bool throw_exceptions=true) {
+    SetFile(filename); 
+    ReadFromFile(throw_exceptions); 
+  }
+  //=============================
+  /* Glean the prefs from the command line.  The "types" argument is a vector of argTypes, which tell the flag, the key to assign, and the type, e.g., 
+     vector<argType> types; 
+     types.push_back("-keep", "keep", "bool"); 
+     types.push_back("-longflag", "longkey", "long"); 
+     types.push_back("-doubleflag", "doublekey", "double"); 
+     types.push_back("-d", "doublekey", "double"); 
+     GetFromArgs(argc, argv, types); 
+
+     When done, the options and their arguments will be stripped from argv and argc will be adjusted appropriately.  
+  */ 
+
+  void GetFromArgs(int &argc, char *argv[], vector<argType> &types);
+
+  //=============================
+  // Copy the entire environment variable list into prefs, e.g., if $verbose is 5, then set Prefs["verbose"] to "5"
+  void ReadFromEnvironment(void); 
+
+  bool hasKey(string key) {
+    return mPrefs.find(key) != mPrefs.end();
+  }
+
+  //=============================
+  // AddEquivalentArgs: (not implemented yet)
+  /* All keys in inEquivalents are considered to be the same as inOption when found on the command line.  inEquivalents are appended to any previous equivalents.  For example, if inOption = "keep" and inEquivalents is { "keep", "dontdiscard" }, then all the following set the value of variable "keep", when found on the command line: {--keep, -keep, --dontdiscard, -dontdiscard }.  Equivalencies can be chained together.  That is, 
+     AddEquivalentArgs("arg", {"blah"});
+     and
+     AddEquivalentArgs("blah", {"blather"}); 
+     makes "blather" an equivalent to "arg"!
+  */
+  // void AddEquivalentArgs(const string &inOption, const vector<string> &inEquivalents); 
+  /* append the inEquivalent to the list of equivalent options for inOption */
+  // void AddEquivalentArg(const string &inOption, const vector<string> &inEquivalents);
+
+
+  //========================
+  /* values: getting and setting (remember to set dirty bit!) 
+     Note that all values are actually saved as strings and are converted, so this is not exactly a high-performance library.  :-) */
+  void SetValue(const std::string &key, const std::string &value) { mPrefs[key]= value;}
+  void SetValue(const std::string &key, double value);
+  void SetBoolValue(const std::string &key, bool value) {
+    SetValue(key, value?string("true"):string("false")); 
+  }
+  void DeleteValue(const std::string &key) {
+    mPrefs.erase(key); // safe if key does not exist. 
+  }
+
+  // The following return false if no such key exists or bad values or an empty string are generated, 
+  // They return true if it does and store the value: 
+  bool TryGetValue(const std::string &key, std::string &outValue) const;
+  bool TryGetLongValue(const std::string &key, long &outValue) const;
+  bool TryGetDoubleValue(const std::string &key, double &outValue) const;
+  // see below for GetBoolValue()
+
+  /*!
+    The following return 0, or "" if no such key exists or bad vals are generated. If dothrow is true, then  throw an exception for bad vals.
+  */ 
+  std::string GetValue(const  std::string &key, bool dothrow=false) const;
+  long GetLongValue(const  std::string &key, bool dothrow=false)  const;
+  double GetDoubleValue(const  std::string &key, bool dothrow=false) const;
+  std::string operator [] (const std::string &key) const { return GetValue(key); }
+
+  // special case: never throws;  not being defined is the same as being false
+  bool GetBoolValue(const std::string &key) { 
+    string value = GetValue(key, false); 
+    return value == "true" || value == "1" || value == "on"; 
+  }
+  
+  void SetValidArgs(std::vector<argType> &args) {
+    mValidArgs = args; 
+  }
+ protected:
+  std::string NextKey(std::ifstream&theFile);
+  std::map<std::string, std::string> ReadNextSection(std::ifstream &theFile);
+
+  bool KeyValid(std::string key); //true if key is in mValidArgs
+
+  void SaveSectionToFile(std::ofstream &outfile, std::map<std::string, std::string> &section);
+
+  // there is no copy constructor, so be sure these shallow copy:
+  //vector< pair< string, vector<string> > > mEquivalents; 
+  std::map<std::string, std::string>  mPrefs; 
+  std::string mFilename;
+  std::vector<argType> mValidArgs; // only save these args or look for them in environment etc. if given
+  char _dirty; 
+  char _writtenToDisk;
+};
+
+#endif
