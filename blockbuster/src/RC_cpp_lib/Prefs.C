b7f810156a17b0bddcab26ec89827884767e0d64
Rich Cook <rcook@llnl.gov>
2014-04-14 16:35:59 -0700

    put all my lib files in one place to avoid confusion
    Former-commit-id: 8d103bb26272c4a34b5cc52ec12eb8d7eaf821a0

---
+++ blockbuster/src/RC_cpp_lib/Prefs.C
@@ -0,0 +1,474 @@
+/* MODIFIED BY: rcook on Fri Apr 11 18:31:40 PDT 2014 */
+/* VERSION: 1.0 */
+#define NO_BOOST 1
+#include "Prefs.h"
+#include "stringutil.h"
+#include "errno.h"
+#include <boost/lexical_cast.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+using boost::property_tree::ptree; 
+
+/* class Preferences
+   member functions 
+*/
+// $Revision: 1.27 $
+
+//#ifdef __USE_GNU
+extern char **environ;
+
+//#endif
+
+using namespace std;
+
+// IF YOU WANT TO DEBUG PREFS, set the "debugprefs" environment variable to 1
+static string debugprefs("false"); 
+#define prefsdebug if (debugprefs != "false") cerr << __FILE__ <<" line "<<__LINE__ << ": "
+
+//====================================================
+ArgType::ArgType(string key,  int autoFlags, int multi, string defaultVal):
+  mKey(key), mType("string"), mMultiple(multi), mValues(1,defaultVal){
+  if (autoFlags) {
+    mFlags.push_back(str(format("--%1%")%key)); 
+    mFlags.push_back(str(format("-%c")%key[0])); 
+  }
+  return;
+ 
+}
+
+//====================================================
+ArgType::ArgType(string key, string preftype,  int autoFlags, 
+                 int multi, string defaultVal):
+  mKey(key), mType(preftype), mMultiple(multi), mValues(1,defaultVal) {
+  if (autoFlags) {
+    mFlags.push_back(str(format("--%1%")%key)); 
+    mFlags.push_back(str(format("-%c")%key[0])); 
+  }
+  return; 
+}
+  
+//====================================================
+ArgType::ArgType(string key, string preftype, vector<string> flags, 
+                 int multi, string defaultVal):
+  mKey(key), mType(preftype), mMultiple(multi), 
+  mFlags(flags), mValues(1,defaultVal) {
+  return; 
+}
+
+//====================================================
+ArgType::ArgType(string key, string preftype, 
+                 string flag1, string flag2, 
+                  int multi, string defaultVal):
+  mKey(key), mType(preftype), mMultiple(multi), 
+  mValues(1,defaultVal) {
+  mFlags.push_back(flag1); 
+  mFlags.push_back(flag2); 
+  return; 
+}
+
+//====================================================
+ArgType::ArgType(string key, string preftype, string flag, 
+                 int multi, string defaultVal):
+  mKey(key), mType(preftype), mMultiple(multi), 
+  mFlags(1,flag), mValues(1,defaultVal) {
+  return; 
+}
+
+//====================================================
+ArgType::ArgType(const ArgType &other) {
+  *this = other; 
+}
+
+//====================================================
+const ArgType &ArgType::operator =(const ArgType &other) {
+  mKey = other.mKey; 
+  mType = other.mType; 
+  mFlags = other.mFlags; 
+  mMultiple = other.mMultiple; 
+  mValues = other.mValues; 
+  return *this; 
+}
+    
+//====================================================
+ArgType::operator string() {
+  string output = str(format("mKey: \"%s\", mMultiple: %d, mType: \"%s\", mFlags: ")% mKey % ((int)mMultiple) % mType); 
+  vector<string>::iterator pos = mFlags.begin(); 
+  if (pos != mFlags.end()) {
+    output += "<"; 
+  }
+  while (pos != mFlags.end()) {
+    output += str(format("\"%s\"")%(*pos)); 
+    if (++pos == mFlags.end()){
+      output += ">"; 
+    } else {
+      output += ", "; 
+    }
+  }
+  return output; 
+}
+
+//====================================================
+void Preferences::Merge(const Preferences &other, bool addnew, bool overwrite) {
+
+  if (!addnew && !overwrite) return; // one must be true for anything to happen
+
+  std::map<string, ArgType >::const_iterator pos = other.mPrefs.begin(), endpos = other.mPrefs.end(), found; 
+  while (pos != endpos) {
+    found = mPrefs.find(pos->first); 
+    if ((addnew && found == mPrefs.end()) ||
+        (overwrite && found != mPrefs.end()) ) {
+      mPrefs[pos->first] = pos->second; 
+    }
+    ++pos; 
+  }
+}
+      
+
+//====================================================
+void Preferences::Reset(void){
+  ClearPrefs();
+
+  char *dbptr = getenv("debugprefs");
+  if (dbptr) {
+    debugprefs = dbptr;
+    cerr << "enabling debugging of prefs because debugprefs environment variable is set" << endl; 
+  }
+ 
+  _dirty = 0;
+  _writtenToDisk = 0;
+  return;
+}
+
+//====================================================
+bool Preferences::TryGetValue(const string &key,  std::string &outValue) const {
+  if (mPrefs.find(key) == mPrefs.end()) {
+    return false; 
+  }
+  outValue = GetValue(key, true);    
+  return (outValue != ""); 
+}
+
+//====================================================
+bool Preferences::TryGetDoubleValue(const string &key, double &outValue) const {
+  try {
+    outValue = GetDoubleValue(key, true); 
+  } catch (...) {
+    outValue = DOUBLE_FALSE;
+    return false; 
+  }
+  return outValue!= DOUBLE_FALSE; 
+}
+
+//====================================================
+bool Preferences::TryGetLongValue(const string &key, long &outValue) const {
+  try {
+    outValue = GetLongValue(key, true);   
+  } catch(...) {
+    outValue = LONG_FALSE;
+    return false; 
+  }
+  return outValue != LONG_FALSE; 
+}
+
+//====================================================
+vector<string> Preferences::GetValues(const std::string &key, bool dothrow) const {
+  if (mPrefs.find(key) == mPrefs.end()) {
+    if (dothrow) {
+      throw string(key+" not found");
+    } else {
+      vector<string> blank(1); 
+      return blank;
+    }
+  }
+  return mPrefs.find(key)->second.mValues;
+}
+
+//====================================================
+string Preferences::GetValue(const std::string &key, bool dothrow) const { 
+  vector<string> values = GetValues(key,dothrow); 
+  return values[0]; 
+}
+
+
+//====================================================
+template <class T> 
+T Preferences::GetValue(const std::string &key, bool dothrow, T &outval) const {
+  string value = GetValue(key, dothrow);
+  try {
+    outval = boost::lexical_cast<T>(value); 
+  } catch (...) {
+    if (dothrow) {
+      throw string("Bad value in  Preferences::GetValue");
+    } 
+  }
+  return outval; 
+}
+
+//====================================================
+template <class T> 
+vector<T> Preferences::GetValues(const std::string &key, bool dothrow, vector<T> &outvals) const {
+  vector<string> values = GetValues(key, dothrow);
+  for ( vector<string>::iterator value = values.begin(); value != values.end(); value++) {
+    try {
+      outvals.push_back( boost::lexical_cast<T>(*value)); 
+    } catch (...) {
+      if (dothrow) {
+        throw string("Bad value in  Preferences::GetValues");
+      } 
+    }
+  }   
+  return outvals; 
+}
+
+//====================================================
+double Preferences::GetDoubleValue(const string &key, bool dothrow) const {
+  double v; 
+  return GetValue(key, dothrow, v); 
+}
+
+//====================================================
+vector<double> Preferences::GetDoubleValues(const string &key, bool dothrow) const {
+  vector<double> v; 
+  return GetValues(key, dothrow, v); 
+}
+
+//====================================================
+long Preferences::GetLongValue(const string &key, bool dothrow) const {
+  long v; 
+  return GetValue(key, dothrow, v); 
+}
+
+//====================================================
+vector<long> Preferences::GetLongValues(const string &key, bool dothrow) const {
+  vector<long> v; 
+  return GetValues(key, dothrow, v); 
+}
+
+//====================================================
+Preferences::operator string()  {
+  string output = "Preferences: { \n"; 
+  output += "  mValidArgs: {";
+  for (vector<ArgType>::iterator argpos = mValidArgs.begin(); argpos != mValidArgs.end(); argpos++) {
+    output += str(format("\n    %s")%(string(*argpos))); 
+  }
+  output += "  }\n"; 
+  output += "  mPrefs: {"; 
+  for (map<string,ArgType>:: iterator pref = mPrefs.begin(); pref != mPrefs.end(); pref++) {
+    output += string(pref->second) + "\n"; 
+  }
+  output += "  }\n"; 
+  return output; 
+}
+
+
+//====================================================
+/* Look in the environment for new values for previously declared keys */ 
+void Preferences::ReadFromEnvironment(void){
+  prefsdebug << "ReadFromEnvironment"<<endl;
+  char **envp = environ; 
+  char *currentstring; 
+  while ((currentstring = *envp) != NULL) {
+    string keypair(currentstring); 
+    string::size_type equals = keypair.find('=');
+    if (equals == string::npos) {
+      throw string("Error in environment:  environment string is malformed: \"") + keypair + "\""; 
+    }    
+    string key = keypair.substr(0, equals), value = keypair.substr(equals+1);
+    prefsdebug << "Setting prefs key "<< key<< " to value from environment: " << value << endl; 
+    mPrefs[key] = value;
+    ++envp; 
+  }
+  return; 
+}
+
+//====================================================
+/* skip comments and get the next valid token */
+string Preferences::NextKey(ifstream&theFile){
+  char buf[2048];
+  string key("");
+  prefsdebug << "Preferences::NextKey"<<endl; 
+  try {
+    while (theFile) {
+      theFile >> key;     
+      if (key.length() && key[0] == '#')     
+        theFile.getline(buf, 2048);
+      else
+        break;
+    }
+  } catch (...) {
+    cerr << "Error in NextKey()" << endl; 
+    throw; 
+  }
+  return key;
+}
+
+ 
+//====================================================
+bool Preferences::SaveToFile(bool createDir, bool clobber) {
+  string filename = GetValue("Filename"); 
+  if (filename == "") 
+    return false; 
+
+  if (createDir) {
+    string::size_type idx = filename.rfind('/'); 
+    if (idx != string::npos) {
+      string dirname = filename.substr(0, idx); 
+      system((string("mkdir -p ")+dirname).c_str()); 
+    }
+  }
+  ptree saved; 
+  if (!clobber) {
+    ifstream infile(filename.c_str()); 
+    
+    if (infile.is_open()) {
+      return false; // file exists; do not overwrite
+    } 
+  }
+
+  // We can now write our json file
+  ptree pt; 
+  for (map<string, ArgType >::iterator pref = mPrefs.begin(); pref != mPrefs.end(); pref++) {
+    pt.put(str(format("%s.mType")%(pref->second.mKey)), pref->second.mType); 
+    pt.put(str(format("%s.mMultiple")%(pref->second.mKey)), 
+           str(format("%d")%((int)(pref->second.mMultiple)))); 
+    if (pref->second.mFlags.size()) {
+      ptree flags;
+      for (vector<string>::iterator flagval = pref->second.mFlags.begin(); 
+           flagval != pref->second.mFlags.end(); flagval++) {
+        ptree flag; 
+        flag.put("", *flagval); 
+        flags.push_back(make_pair("", flag)); 
+      }
+      pt.add_child(str(format("%s.mFlags")% (pref->second.mKey)), flags); 
+    }
+    if (pref->second.mValues.size()) {
+      ptree values; 
+      for (vector<string>::iterator valueval = pref->second.mValues.begin(); 
+           valueval != pref->second.mValues.end(); valueval++) {
+        ptree value; 
+        value.put("", *valueval); 
+        values.push_back(make_pair("", value)); 
+      }
+      pt.add_child(str(format("%s.mValues")% (pref->second.mKey)), values); 
+    }
+  }
+  write_json(filename, pt); 
+  return true; 
+}
+
+//====================================================
+bool Preferences::ReadFromFile(bool throw_exceptions) {
+   try {
+     ptree pt;
+     read_json(GetValue("Filename"), pt); 
+     if (pt.empty()) {
+       return false; 
+     }
+     for (ptree::iterator pos = pt.begin(); pos != pt.end(); pos++) {  
+       string key = pos->first; 
+       ArgType arg(key); 
+       for (ptree::iterator child = pos->second.begin(); child != pos->second.end(); child++) {
+         ptree childtree = child->second;
+         if (child->first == "mType") {
+           arg.mType = childtree.data(); 
+         } else if (child->first == "mMultiple") {
+           arg.mMultiple = lexical_cast<int>(childtree.data()); 
+         } else if (child->first == "mFlags") {
+           for (ptree::iterator flag = childtree.begin(); flag != childtree.end(); flag++) {
+             arg.mFlags.push_back(flag->second.data()); 
+           }
+         } else if (child->first == "mValues") {
+           for (ptree::iterator value = childtree.begin(); value != childtree.end(); value++) {
+             arg.mValues.push_back(value->second.data()); 
+           }
+         }
+       }
+       mPrefs[arg.mKey] = arg; 
+     }
+   } catch (...) {
+     cerr << "Exception in ReadFromFile" << endl;      
+     if (throw_exceptions) {
+       throw; 
+     } 
+     return false; 
+   }
+   return true; 
+}
+
+
+// ================================================================== 
+void ConsumeArg(int argnum, int &argc, char *argv[]){
+  while (argnum < argc-1) {
+    argv[argnum] = argv[argnum+1];
+    ++argnum; 
+  }
+  argv[argnum] = NULL; 
+  argc--; 
+}
+
+// ================================================================== 
+void Preferences::GetFromArgs(int &argc, char *argv[], vector<ArgType>& args, bool rejectUnknown) {
+  
+  // first, initialize all keys in "argtypes" array to defaults if not already set to some value
+  AddArgs(args); 
+  ParseArgs(argc, argv, rejectUnknown); 
+  return; 
+}
+      
+// ================================================================== 
+void Preferences::ParseArgs(int &argc, char *argv[], bool rejectUnknown) {
+  // parse the argc and argv as command line options.
+  // 
+  errno = 0; 
+  int argnum = 0; 
+  string foundflag; 
+  while (argnum < argc) {
+    string currentArg = argv[argnum]; 
+    vector<ArgType>::iterator validArg; 
+    for (validArg = mValidArgs.begin();  
+         validArg != mValidArgs.end(); 
+         ++validArg) {
+      for (vector<string>::iterator flagpos = validArg->mFlags.begin(); 
+           flagpos != validArg->mFlags.end();
+           flagpos++) {
+        if (*flagpos == currentArg) {
+          foundflag = *flagpos;   
+          break; 
+        }
+      }
+      if (foundflag != "") break; 
+    }
+
+    if (foundflag == "") {
+      if (rejectUnknown && currentArg[0] == '-') {
+        throw str(format("Unknown argument %s ")%currentArg); 
+      }        
+      argnum++; 
+    } else {
+      // do not increment argnum here as you will simply be consuming args and reducing argc
+      ConsumeArg(argnum, argc,argv); 
+      if (validArg->mType == "bool") {
+        SetBoolValue(validArg->mKey, true); 
+      } else {
+        if (argnum == argc) {
+          throw string("Flag ")+foundflag+string(" requires an argument");
+        }     
+        string value = argv[argnum];
+        try {
+          if (validArg->mType == "string") {
+            SetValue(validArg->mKey, value);
+          } else if (validArg->mType == "long") {
+            SetLongValue(validArg->mKey, boost::lexical_cast<long>(value));
+          } else if (validArg->mType == "double") {
+            SetDoubleValue(validArg->mKey,  boost::lexical_cast<double>(value)); 
+          } else {
+            throw string("Bad keytype: ")+validArg->mType+string(" for key: ")+validArg->mKey; 
+          }
+        } catch (...) {
+          throw str(format("Could not convert value %s to type %s for flag %s") % value % validArg->mType % foundflag); 
+        }
+      }// end check which arg type it is
+      ConsumeArg(argnum, argc,argv); 
+    } /* end  if (found) */
+  } // end while (argnum < argc)
+} // end ParseArgs()
---
+++ blockbuster/src/RC_cpp_lib/Prefs.h
@@ -0,0 +1,271 @@
+/* MODIFIED BY: rcook on Fri Apr 11 18:31:40 PDT 2014 */
+/* VERSION: 1.0 */
+/* This file is an attempt to allow any application to read its preferences into what Randy Frank would call a "mapobj".  I have stolen his idea and hopefully improved it to be more general and more robust, because it no longer relies on pointers to store its information.  In fact, it no longer allows pointers to be stored at all.  The presumption is that this is non-volatile information which can be written to disk.  I don't know of a way yet to pickle C items.  Maybe later I'll change to a binary output format and then allow any data to be captured to disk.  Not today. 
+   All values are stored as C++ strings.  Functions which set or get values as other types are merely converting a string to the desired type or vice-versa.
+
+   To do:  multiple Preferences should be able to be written to a single file. 
+ */
+// $Revision: 1.20 $
+#ifndef RCPREFS_H
+#define RCPREFS_H
+
+#include <map>
+#include <iostream>
+#include <vector> 
+#include <string>
+#include <fstream>
+#include "stringutil.h"
+#include <boost/format.hpp>
+
+#define DOUBLE_FALSE (0.0)
+#define LONG_FALSE  (0)
+using namespace boost; 
+
+
+//======================================================
+struct ArgType {
+
+  ArgType(string key=""): mKey(key), mType("string") {}
+  ArgType(string key,  int autoFlags, int multi, string defaultVal);
+
+  ArgType(string key, string preftype,  int autoFlags,  
+          int multi=false, string defaultVal="");
+  
+  ArgType(string key,  string preftype, vector<string> flags, 
+          int multi=false, string defaultVal="");
+
+  ArgType(string key, string preftype, 
+          string flag1, string flag2, 
+          int multi=false, string defaultVal="");
+
+  ArgType(string key, string preftype, string flag, 
+          int multi=false, string defaultVal="");
+
+  ArgType(const ArgType &other);
+
+  const ArgType &operator =(const ArgType &other);
+    
+  operator string();
+
+  bool operator !=(const ArgType &other) { 
+    return !( *this == other); 
+  }
+  bool operator == (const ArgType &other) {
+    return (mKey == other.mKey &&
+            mType == other.mType && 
+            mMultiple == other.mMultiple &&
+            mFlags == other.mFlags && 
+            mValues == other.mValues); 
+  }
+
+  string mKey, 
+    mType; // "bool", "long", "double", "string"
+  bool mMultiple; // interpret as bool
+  vector<string> mFlags; // aliasable e.g. "--help" and "-h" 
+  vector<string> mValues; 
+};
+
+//======================================================
+typedef  std::map<std::string, ArgType> PrefsMap; 
+
+//======================================================
+class Preferences {
+ public:
+  //========================
+  //Initialization
+  Preferences(string filename = ""){
+    Reset();
+    SetValue("Filename", filename); 
+  }
+  
+  /* Merge:  combine this with other.  
+     Iff addnew is true, then keys in other but not in this will be added to this
+     Iff overwrite is true, then keys in other overwrite any keys in this by the same name.
+     Note that if both are false, then nothing will be done! 
+  */
+  void Merge(const Preferences &other, bool addnew=true, bool overwrite=true); 
+  // conveniences:
+  void MergeNoOverwrite(const Preferences &other) { Merge(other, true, false); }
+  void MergeNoAdd(const Preferences &other) {Merge(other, false, true); }
+
+  void Reset(void); 
+  void ClearPrefs(void) { mPrefs.clear();}
+  std::map<std::string, ArgType > GetPrefsMap(void) { return mPrefs; }
+
+  //========================
+  // meta-attributes
+
+
+  void SetFile(string filename) {
+    SetValue("Filename", filename); 
+  }
+  string GetFilename(void) {
+    return GetValue("Filename"); 
+  }
+  //========================
+  //saving and restoring from disk
+
+ //====================================================
+  bool ReadFromFile(bool throw_exceptions=true); 
+ 
+  bool SaveToFile(bool createDir=true, bool clobber=true); 
+  //=============================
+  /* Glean the prefs from the command line.  The "types" argument is a vector of ArgTypes, which tell the flag, the key to assign, and the type, e.g., 
+     vector<ArgType> types; 
+     types.push_back("-keep", "keep", "bool"); 
+     types.push_back("-longflag", "longkey", "long"); 
+     types.push_back("-doubleflag", "doublekey", "double"); 
+     types.push_back("-d", "doublekey", "double"); 
+     GetFromArgs(argc, argv, types); 
+
+     When done, the options and their arguments will be stripped from argv and argc will be adjusted appropriately.  
+  */ 
+  
+  //====================================================
+  PrefsMap::iterator begin(void) { 
+    return mPrefs.begin(); 
+  }
+
+  PrefsMap::iterator end(void) { 
+    return mPrefs.end(); 
+  }
+  //====================================================
+  Preferences &AddArg(ArgType arg) {
+    // first look for duplicates: 
+    for (vector<ArgType>::iterator argPos = mValidArgs.begin();  
+         argPos != mValidArgs.end(); 
+         ++argPos) {
+      if (arg.mKey == argPos->mKey) {
+        throw str(format("AddArg Error: duplicate key: %s")%arg.mKey);
+      }
+      for (vector<string>::iterator flag = argPos->mFlags.begin(); 
+           flag != argPos->mFlags.end(); flag++) {
+        if (find(arg.mFlags.begin(), arg.mFlags.end(), *flag) != arg.mFlags.end()) {
+          throw str(format("AddArg Error: duplicate flag: %s")%(*flag));
+        }
+      }
+    }
+  
+    // now add a value in Prefs by type
+    mPrefs[arg.mKey] = arg; 
+    
+    return *this; 
+  }
+  
+  //====================================================
+  Preferences &AddArgs(std::vector<ArgType> &args) {
+    for (vector<ArgType>::iterator argPos = args.begin(); argPos != args.end(); ++argPos) {
+      AddArg(*argPos); 
+    }
+    return *this; 
+  }
+  
+  //====================================================
+  // for backwards compatibility
+  Preferences &SetValidArgs(std::vector<ArgType> &args) {
+    AddArgs(args); 
+    return *this; 
+  }
+  
+  //====================================================
+  void GetFromArgs(int &argc, char *argv[], vector<ArgType> &argtypes, bool rejectUnknown=true);
+
+  //====================================================
+  void ParseArgs(int &argc, char *argv[], bool rejectUnknown=true); 
+  //=============================
+  // Copy the entire environment variable list into prefs, e.g., if $verbose is 5, then set Prefs["verbose"] to "5"
+  void ReadFromEnvironment(void); 
+
+  bool hasKey(string key) {
+    return mPrefs.find(key) != mPrefs.end();
+  }
+
+  //=============================
+  // AddEquivalentArgs: (not implemented yet)
+  /* All keys in inEquivalents are considered to be the same as inOption when found on the command line.  inEquivalents are appended to any previous equivalents.  For example, if inOption = "keep" and inEquivalents is { "keep", "dontdiscard" }, then all the following set the value of variable "keep", when found on the command line: {--keep, -keep, --dontdiscard, -dontdiscard }.  Equivalencies can be chained together.  That is, 
+     AddEquivalentArgs("arg", {"blah"});
+     and
+     AddEquivalentArgs("blah", {"blather"}); 
+     makes "blather" an equivalent to "arg"!
+  */
+  // void AddEquivalentArgs(const string &inOption, const vector<string> &inEquivalents); 
+  /* append the inEquivalent to the list of equivalent options for inOption */
+  // void AddEquivalentArg(const string &inOption, const vector<string> &inEquivalents);
+
+
+  //========================
+  /* values: getting and setting (remember to set dirty bit!) 
+     Note that all values are actually saved as strings and are converted, so this is not exactly a high-performance library.  :-) */
+  void SetValue( std::string key, string value, bool autoFlags=false) {
+    mPrefs[key] = ArgType(key, "string", autoFlags, false, value); 
+  }
+  void SetLongValue( std::string key, long value, bool autoFlags=false) {
+    mPrefs[key] = ArgType(key, "long", autoFlags, false, str(format("%d")%value)); 
+  }
+  void SetDoubleValue( std::string key, double value, bool autoFlags=false) {
+    mPrefs[key] = ArgType(key, "double", autoFlags, false, str(format("%f")%value)); 
+  }
+  void SetBoolValue( std::string key, bool value, bool autoFlags=false) {
+    
+    mPrefs[key] = ArgType(key, "bool", autoFlags, false, str(format("%d")%((int)value))); 
+  }
+  void DeleteValue( std::string key) {
+    mPrefs.erase(key); // safe if key does not exist. 
+  }
+
+  // The following return false if no such key exists or bad values or an empty string are generated, 
+  // They return true if it does and store the value: 
+  bool TryGetValue(const std::string &key, std::string &outValue) const;
+  bool TryGetLongValue(const std::string &key, long &outValue) const;
+  bool TryGetDoubleValue(const std::string &key, double &outValue) const;
+  // see below for GetBoolValue()
+
+  /*!
+    The following return 0, or "" if no such key exists or bad vals are generated. If dothrow is true, then  throw an exception for bad vals.
+  */ 
+  std::string GetValue(const  std::string &key, bool dothrow=false) const;
+  vector<string> GetValues(const  std::string &key, bool dothrow=false) const;
+
+  template <class T> 
+    T GetValue(const std::string &key, bool dothrow, T &outval) const; 
+
+  template <class T> 
+    vector<T> GetValues(const std::string &key, bool dothrow, vector<T> &outvals) const; 
+
+  long GetLongValue(const std::string &key, bool dothrow=false)  const;
+  double GetDoubleValue(const  std::string &key, bool dothrow=false) const;
+
+  vector<long> GetLongValues(const std::string &key, bool dothrow=false)  const;
+  vector<double> GetDoubleValues(const  std::string &key, bool dothrow=false) const;
+
+  ArgType GetArg(string key)  { return mPrefs[key]; }
+
+  std::string operator [] (const std::string &key) const { 
+    return GetValue(key); 
+  }
+
+  // special case: never throws;  not being defined is the same as being false
+  bool GetBoolValue(const std::string &key) { 
+    string value = GetValue(key, false); 
+    return value == "true" || value == "1" || value == "on"; 
+  }
+
+  operator string() ;
+
+ protected:
+  std::string NextKey(std::ifstream&theFile);
+  std::map<std::string, ArgType > ReadNextSection(std::ifstream &theFile);
+
+  bool KeyValid(std::string key); //true if key is in mValidArgs
+
+  void SaveSectionToFile(std::ofstream &outfile, std::map<std::string, ArgType > &section);
+
+  // there is no copy constructor, so be sure these shallow copy:
+  //vector< pair< string, vector<string> > > mEquivalents; 
+  std::map<std::string, ArgType >  mPrefs; 
+  std::vector<ArgType> mValidArgs; // only save these args or look for them in environment etc. if given
+  char _dirty; 
+  char _writtenToDisk;
+};
+
+#endif
